a2de3f93bf08e24941036dcffb68fef58c23db79
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2019-11-12 19:05:00 -1000

    Implement `[[trivially_relocatable]]` and `__is_trivially_relocatable(T)`.
    Also implement `__has_extension(trivially_relocatable)` so that the
    library can detect whether the feature exists.
    
    Add attribute [[clang::maybe_trivially_relocatable]].
    
    Add attribute [[trivially_relocatable(bool)]].
    
    Update the implementation to match D1144R6.
    - User-provided special members now prevent natural trivial relocatability.
    - Volatile and mutable members no longer prevent natural trivial relocatability.
    - Being polymorphic now unconditionally prevents natural trivial relocatability.
    
    All tests pass.

--- clang/lib/Sema/SemaExprCXX.cpp
+++ clang/lib/Sema/SemaExprCXX.cpp
@@ -5115,7 +5115,6 @@ static bool EvaluateUnaryTypeTrait(Sema &Self, TypeTrait UTT,
       }
     }
     return true;
-
   case UTT_HasTrivialDestructor:
     // http://gcc.gnu.org/onlinedocs/gcc/Type-Traits.html
     //   If __is_pod (type) is true or type is a reference type
@@ -5267,7 +5266,20 @@ static bool EvaluateUnaryTypeTrait(Sema &Self, TypeTrait UTT,
   case UTT_HasUniqueObjectRepresentations:
     return C.hasUniqueObjectRepresentations(T);
   case UTT_IsTriviallyRelocatable:
-    return T.isTriviallyRelocatableType(C);
+    if (!T.isTriviallyRelocatableType(C))
+      return false;
+    if (auto *Record = T->getAsCXXRecordDecl()) {
+      // A move-constructible, destructible object type [...]
+      Sema::SpecialMemberOverloadResult SMOR = Self.LookupSpecialMember(
+        Record, Sema::CXXDestructor, false, false, false, false, false);
+      if (SMOR.getKind() != Sema::SpecialMemberOverloadResult::Success)
+        return false;
+      SMOR = Self.LookupSpecialMember(
+        Record, Sema::CXXMoveConstructor, false, false, false, false, false);
+      if (SMOR.getKind() != Sema::SpecialMemberOverloadResult::Success)
+        return false;
+    }
+    return true;
   }
 }
 
