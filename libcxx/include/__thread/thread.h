66e4bc4923573488298a9e7dbde4d643ad83a21b
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2019-11-16 15:03:01 -0500

    [libc++] Trivially relocatable (2/3): Add explicit warrants to std library types.
    All this stuff uses the name `__libcpp_is_trivially_relocatable`
    instead of `std::is_trivially_relocatable`.
    
    PMR types are marked as trivially relocatable only if
    `_LIBCPP_TRIVIALLY_RELOCATABLE_PMR_CONTAINERS` is defined to 1 at compile time.
    By default, they're not.

--- libcxx/include/__thread/thread.h
+++ libcxx/include/__thread/thread.h
@@ -130,7 +130,7 @@ basic_ostream<_CharT, _Traits>&
 operator<<(basic_ostream<_CharT, _Traits>& __os, __thread_id __id)
 {return __os << __id.__id_;}
 
-class _LIBCPP_EXPORTED_FROM_ABI thread
+class _LIBCPP_EXPORTED_FROM_ABI _LIBCPP_TRIVIALLY_RELOCATABLE thread
 {
     __libcpp_thread_t __t_;
 
