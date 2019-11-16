904b1cb1d6333a1631a907afe42f9297d9b56510
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2019-11-16 15:00:57 -0500

    [libc++] Trivially relocatable (1/3): Traits and algorithms.
    This implements the library behavior proposed in D1144R6.
    This patch doesn't do any special optimizations (e.g. to `std::vector`);
    those will come later. This is just the bare minimum to conform to
    P1144's additions to the standard.
    
    We mangle all the P1144 names behind `__libcpp_...`, so that they
    aren't taken by the user-programmer to be official Standard C++ names.
    
        struct is_relocatable<T>
        struct is_nothrow_relocatable<T>
        struct is_trivially_relocatable<T>
    
        bool is_relocatable_v<T>
        bool is_nothrow_relocatable_v<T>
        bool is_trivially_relocatable_v<T>
    
        T relocate(T*)
        T* relocate_at(T*, T*)
        InputIt uninitialized_relocate(InputIt, InputIt, ForwardIt)
        pair<InputIt, ForwardIt> uninitialized_relocate_n(InputIt, Size, ForwardIt)

---
+++ libcxx/include/__memory/uninitialized_relocate.h
@@ -0,0 +1,186 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCPP___MEMORY_UNINITIALIZED_RELOCATE_H
+#define _LIBCPP___MEMORY_UNINITIALIZED_RELOCATE_H
+
+#include <__config>
+#include <__iterator/iterator_traits.h>
+#include <__memory/addressof.h>
+#include <__memory/construct_at.h>
+#include <__memory/pointer_traits.h>
+#include <__memory/relocate_at.h>
+#include <__type_traits/integral_constant.h>
+#include <__type_traits/is_nothrow_constructible.h>
+#include <__type_traits/is_trivially_relocatable.h>
+#include <__type_traits/is_volatile.h>
+#include <__type_traits/remove_cvref.h>
+#include <__utility/move.h>
+#include <__utility/pair.h>
+
+#if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
+#  pragma GCC system_header
+#endif
+
+_LIBCPP_BEGIN_NAMESPACE_STD
+
+template<class _InputIt, class _ForwardIt>
+inline _LIBCPP_INLINE_VISIBILITY
+_ForwardIt
+__uninitialized_relocate_impl(_InputIt __first, _InputIt __last,
+                              _ForwardIt __result, false_type, false_type)
+{
+    _ForwardIt __orig_result = __result;
+    for (; __first != __last; ++__result, void(), ++__first) {
+#ifndef _LIBCPP_HAS_NO_EXCEPTIONS
+        try {
+#endif
+        std::__libcpp_relocate_at2(0, std::addressof(*__first), std::addressof(*__result));
+#ifndef _LIBCPP_HAS_NO_EXCEPTIONS
+        } catch (...) {
+            std::__destroy(__orig_result, __result);
+            ++__first;
+            std::__destroy(__first, __last);
+            throw;
+        }
+#endif
+    }
+    return __result;
+}
+
+template<class _InputIt, class _ForwardIt>
+inline _LIBCPP_INLINE_VISIBILITY
+_ForwardIt
+__uninitialized_relocate_impl(_InputIt __first, _InputIt __last,
+                              _ForwardIt __result, false_type, true_type)
+{
+    for (; __first != __last; ++__result, void(), ++__first) {
+        std::__libcpp_relocate_at2(0, std::addressof(*__first), std::addressof(*__result));
+    }
+    return __result;
+}
+
+template<class _InputIt, class _ForwardIt, class _LoopWithoutCatchIgnored>
+inline _LIBCPP_INLINE_VISIBILITY
+_ForwardIt
+__uninitialized_relocate_impl(_InputIt __first, _InputIt __last,
+                              _ForwardIt __result, true_type, _LoopWithoutCatchIgnored)
+{
+    auto __count = __last - __first;
+    if (__count != 0) {
+        char *__firstbyte = (char *)std::__to_address(__first);
+        auto __nbytes = (char *)std::__to_address(__last) - __firstbyte;
+        ::__builtin_memmove(std::__to_address(__result), __firstbyte, __nbytes);
+        __result += __count;
+    }
+    return __result;
+}
+
+template<class _InputIt, class _ForwardIt>
+inline _LIBCPP_INLINE_VISIBILITY
+_ForwardIt
+__libcpp_uninitialized_relocate(_InputIt __first, _InputIt __last,
+                                _ForwardIt __result)
+{
+    using _St = decltype(std::move(*__first));
+    using _Dt = typename iterator_traits<_ForwardIt>::value_type;
+    using _ElementTypeIsMemcpyable = integral_constant<bool,
+        __is_same_uncvref<_St, _Dt>::value &&
+        __libcpp_is_trivially_relocatable<_Dt>::value &&
+        !is_volatile<_St>::value && !is_volatile<_Dt>::value
+    >;
+    using _SingleMemcpy = integral_constant<bool,
+        _ElementTypeIsMemcpyable::value &&
+        __is_cpp17_contiguous_iterator<_InputIt>::value &&
+        __is_cpp17_contiguous_iterator<_ForwardIt>::value
+    >;
+    using _LoopWithoutCatch = is_nothrow_constructible<_Dt, _St>;
+    return std::__uninitialized_relocate_impl(__first, __last, __result,
+        _SingleMemcpy(), _LoopWithoutCatch());
+}
+
+// __libcpp_uninitialized_relocate_n
+
+template<class _InputIt, class _Size, class _ForwardIt>
+inline _LIBCPP_INLINE_VISIBILITY
+pair<_InputIt, _ForwardIt>
+__uninitialized_relocate_n_impl(_InputIt __first, _Size __n,
+                                _ForwardIt __result, false_type, false_type)
+{
+    _ForwardIt __orig_result = __result;
+    for (; __n > 0; ++__result, void(), ++__first, void(), --__n) {
+#ifndef _LIBCPP_HAS_NO_EXCEPTIONS
+        try {
+#endif
+            std::__libcpp_relocate_at2(0, std::addressof(*__first), std::addressof(*__result));
+#ifndef _LIBCPP_HAS_NO_EXCEPTIONS
+        } catch (...) {
+            std::__destroy(__orig_result, __result);
+            for (++__first, void(), --__n; __n > 0; ++__first, void(), --__n) {
+                std::__destroy_at(std::addressof(*__first));
+            }
+            throw;
+        }
+#endif
+    }
+    return {__first, __result};
+}
+
+template<class _InputIt, class _Size, class _ForwardIt>
+inline _LIBCPP_INLINE_VISIBILITY
+pair<_InputIt, _ForwardIt>
+__uninitialized_relocate_n_impl(_InputIt __first, _Size __n,
+                                _ForwardIt __result, false_type, true_type)
+{
+    for (; __n > 0; ++__result, ++__first, void(), --__n) {
+        std::__libcpp_relocate_at2(0, std::addressof(*__first), std::addressof(*__result));
+    }
+    return {__first, __result};
+}
+
+template<class _InputIt, class _Size, class _ForwardIt, class _LoopWithoutCatchIgnored>
+inline _LIBCPP_INLINE_VISIBILITY
+pair<_InputIt, _ForwardIt>
+__uninitialized_relocate_n_impl(_InputIt __first, _Size __n,
+                                _ForwardIt __result, true_type, _LoopWithoutCatchIgnored)
+{
+    using _Dt = typename iterator_traits<_ForwardIt>::value_type;
+    if (__n != 0) {
+        ::__builtin_memmove(std::addressof(*__result), std::addressof(*__first), __n * sizeof (_Dt));
+        __first += __n;
+        __result += __n;
+    }
+    return {__first, __result};
+}
+
+template<class _InputIt, class _Size, class _ForwardIt>
+inline _LIBCPP_INLINE_VISIBILITY
+pair<_InputIt, _ForwardIt>
+__libcpp_uninitialized_relocate_n(_InputIt __first, _Size __n, _ForwardIt __result)
+{
+    using _St = decltype(std::move(*__first));
+    using _Dt = typename iterator_traits<_ForwardIt>::value_type;
+    using _ElementTypeIsMemcpyable = integral_constant<bool,
+        __is_same_uncvref<_St, _Dt>::value &&
+        __libcpp_is_trivially_relocatable<_Dt>::value &&
+        !is_volatile<_St>::value && !is_volatile<_Dt>::value
+    >;
+    using _SingleMemcpy = integral_constant<bool,
+        _ElementTypeIsMemcpyable::value &&
+        __is_cpp17_contiguous_iterator<_InputIt>::value &&
+        __is_cpp17_contiguous_iterator<_ForwardIt>::value
+    >;
+    using _LoopWithoutCatch = is_nothrow_constructible<_Dt, _St>;
+    return std::__uninitialized_relocate_n_impl(__first, __n, __result,
+        _SingleMemcpy(), _LoopWithoutCatch());
+}
+
+_LIBCPP_END_NAMESPACE_STD
+
+#endif // _LIBCPP___MEMORY_UNINITIALIZED_RELOCATE_H
---
+++ libcxx/test/libcxx/type_traits/is_trivially_relocatable.compile.pass.cpp
@@ -0,0 +1,275 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// UNSUPPORTED: c++98, c++03, c++11, c++14
+//
+// <type_traits>
+//
+// Test that many standard types are trivially relocatable.
+
+#include "test_macros.h"
+
+#include <any>
+#include <array>
+#include <atomic>
+#include <barrier>
+#include <bitset>
+#include <chrono>
+#include <condition_variable>
+#include <compare>
+#include <deque>
+//#include <flat_map>
+//#include <flat_set>
+#include <forward_list>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <ios>
+#include <iterator>
+//#include <jthread>
+#include <latch>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <memory_resource>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <random>
+#include <ranges>
+#include <ratio>
+#include <regex>
+#include <semaphore>
+#include <set>
+#include <shared_mutex>
+#include <source_location>
+#include <span>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <variant>
+#include <vector>
+
+
+struct T { T(); T(const T&); ~T(); };
+struct R { };
+struct SM { void lock_shared(); void unlock_shared(); ~SM(); };
+
+struct K {
+    T t;
+    bool operator==(const K&) const;
+    bool operator< (const K&) const;
+    struct Cmp {
+        Cmp(); Cmp(const Cmp&); ~Cmp();
+        bool operator()(const K&, const K&) const;
+    };
+};
+template<> struct std::hash<K> { size_t operator()(const K&) const; };
+
+static const bool NotDebug =
+#ifdef _LIBCPP_ENABLE_DEBUG_MODE
+    false;
+#else
+    true;
+#endif
+
+// Define the P1144 name for convenience.
+#define is_trivially_relocatable_v __libcpp_is_trivially_relocatable_v
+
+static_assert(std::is_trivially_relocatable_v<R>);
+static_assert(!std::is_trivially_relocatable_v<T>);
+static_assert(std::is_trivially_relocatable_v<T*>);
+static_assert(std::is_trivially_relocatable_v<std::reference_wrapper<T>>);
+static_assert(std::is_trivially_relocatable_v<std::initializer_list<T>>);
+static_assert(std::is_trivially_relocatable_v<std::allocator<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::pmr::memory_resource>);  // virtual destructor
+static_assert(std::is_trivially_relocatable_v<std::pmr::polymorphic_allocator<T>>);
+static_assert(std::is_trivially_relocatable_v<std::default_delete<T>>);
+static_assert(std::is_trivially_relocatable_v<std::hash<std::string>>);
+static_assert(std::is_trivially_relocatable_v<std::less<T>>);
+static_assert(std::is_trivially_relocatable_v<std::less<void>>);
+static_assert(!std::is_trivially_relocatable_v<std::unique_ptr<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::shared_ptr<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::weak_ptr<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::pair<R,R>>);
+static_assert(!std::is_trivially_relocatable_v<std::pair<R,T>>);
+static_assert(!std::is_trivially_relocatable_v<std::tuple<R,R,R>>);
+static_assert(!std::is_trivially_relocatable_v<std::tuple<R,T,R>>);
+static_assert(std::is_trivially_relocatable_v<std::variant<R,R,R>>);
+static_assert(!std::is_trivially_relocatable_v<std::variant<R,T,R>>);
+static_assert(std::is_trivially_relocatable_v<std::optional<R>>);
+static_assert(!std::is_trivially_relocatable_v<std::optional<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::any>);
+static_assert(!std::is_trivially_relocatable_v<std::locale>);
+static_assert(!std::is_trivially_relocatable_v<std::exception_ptr>);
+static_assert(!std::is_trivially_relocatable_v<std::exception>);  // virtual destructor
+static_assert(!std::is_trivially_relocatable_v<std::error_category>);
+static_assert(std::is_trivially_relocatable_v<std::error_code>);
+static_assert(std::is_trivially_relocatable_v<std::error_condition>);
+static_assert(std::is_trivially_relocatable_v<std::errc>);
+static_assert(std::is_trivially_relocatable_v<std::type_index>);
+static_assert(std::is_trivially_relocatable_v<std::array<R, 5>>);
+static_assert(!std::is_trivially_relocatable_v<std::array<T, 5>>);
+static_assert(!std::is_trivially_relocatable_v<std::deque<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::forward_list<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::list<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::map<K,T>>);
+static_assert(!std::is_trivially_relocatable_v<std::multimap<K,T>>);
+static_assert(!std::is_trivially_relocatable_v<std::multiset<K>>);
+static_assert(!std::is_trivially_relocatable_v<std::set<K>>);
+static_assert(std::is_trivially_relocatable_v<std::unordered_map<K,T>> == false);
+static_assert(std::is_trivially_relocatable_v<std::unordered_multimap<K,T>> == false);
+static_assert(std::is_trivially_relocatable_v<std::unordered_multiset<K>> == false);
+static_assert(std::is_trivially_relocatable_v<std::unordered_set<K>> == false);
+static_assert(std::is_trivially_relocatable_v<std::vector<T>> == false);
+static_assert(!std::is_trivially_relocatable_v<std::vector<bool>>);
+static_assert(!std::is_trivially_relocatable_v<std::stack<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::stack<T, std::list<T>>>);
+static_assert(!std::is_trivially_relocatable_v<std::queue<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::queue<T, std::list<T>>>);
+static_assert(std::is_trivially_relocatable_v<std::priority_queue<K>> == false);
+static_assert(!std::is_trivially_relocatable_v<std::priority_queue<K, std::deque<K>>>);
+static_assert(!std::is_trivially_relocatable_v<std::priority_queue<K, std::deque<K>, K::Cmp>>);
+static_assert(!std::is_trivially_relocatable_v<std::pmr::deque<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::pmr::forward_list<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::pmr::list<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::pmr::map<K,T>>);
+static_assert(!std::is_trivially_relocatable_v<std::pmr::multimap<K,T>>);
+static_assert(!std::is_trivially_relocatable_v<std::pmr::multiset<K>>);
+static_assert(!std::is_trivially_relocatable_v<std::pmr::set<K>>);
+static_assert(std::is_trivially_relocatable_v<std::pmr::unordered_map<K,T>> == false);
+static_assert(std::is_trivially_relocatable_v<std::pmr::unordered_multimap<K,T>> == false);
+static_assert(std::is_trivially_relocatable_v<std::pmr::unordered_multiset<K>> == false);
+static_assert(std::is_trivially_relocatable_v<std::pmr::unordered_set<K>> == false);
+static_assert(std::is_trivially_relocatable_v<std::pmr::vector<T>> == false);
+static_assert(!std::is_trivially_relocatable_v<std::pmr::vector<bool>>);
+static_assert(std::is_trivially_relocatable_v<std::array<R, 5>::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::array<T, 5>::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::deque<T>::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::forward_list<T>::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::list<T>::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::map<K,T>::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::multimap<K,T>::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::multiset<K>::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::set<K>::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::unordered_map<K,T>::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::unordered_multimap<K,T>::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::unordered_multiset<K>::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::unordered_set<K>::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::unordered_map<K,T>::local_iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::unordered_multimap<K,T>::local_iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::unordered_multiset<K>::local_iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::unordered_set<K>::local_iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::vector<T>::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::vector<T>::const_iterator> == NotDebug);
+static_assert(!std::is_trivially_relocatable_v<std::vector<bool>::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::vector<bool>::const_iterator>);
+static_assert(std::is_trivially_relocatable_v<std::reverse_iterator<int*>>);
+static_assert(std::is_trivially_relocatable_v<std::back_insert_iterator<std::deque<int>>>);
+static_assert(std::is_trivially_relocatable_v<std::front_insert_iterator<std::list<int>>>);
+static_assert(std::is_trivially_relocatable_v<std::insert_iterator<std::vector<int>>> == NotDebug);
+static_assert(!std::is_trivially_relocatable_v<std::insert_iterator<std::vector<bool>>>);
+static_assert(std::is_trivially_relocatable_v<std::istream_iterator<R>>);
+static_assert(!std::is_trivially_relocatable_v<std::istream_iterator<T>>);
+static_assert(std::is_trivially_relocatable_v<std::ostream_iterator<R>>);
+static_assert(std::is_trivially_relocatable_v<std::ostream_iterator<T>>);
+static_assert(std::is_trivially_relocatable_v<std::char_traits<char>>);
+static_assert(!std::is_trivially_relocatable_v<std::regex_traits<char>>);  // contains a locale
+static_assert(!std::is_trivially_relocatable_v<std::regex>);  // contains a regex_traits
+static_assert(std::is_trivially_relocatable_v<std::cmatch> == false);  // contains a vector
+static_assert(std::is_trivially_relocatable_v<std::smatch> == false);  // contains a vector
+static_assert(std::is_trivially_relocatable_v<std::string> == false);
+static_assert(std::is_trivially_relocatable_v<std::string::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::wstring> == false);
+static_assert(std::is_trivially_relocatable_v<std::wstring::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::string_view>);
+static_assert(std::is_trivially_relocatable_v<std::string_view::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::wstring_view>);
+static_assert(std::is_trivially_relocatable_v<std::wstring_view::iterator>);
+static_assert(std::is_trivially_relocatable_v<std::pmr::cmatch> == false);  // contains a vector
+static_assert(std::is_trivially_relocatable_v<std::pmr::smatch> == false);  // contains a vector
+static_assert(std::is_trivially_relocatable_v<std::pmr::string> == false);
+static_assert(std::is_trivially_relocatable_v<std::pmr::string::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::pmr::wstring> == false);
+static_assert(std::is_trivially_relocatable_v<std::pmr::wstring::iterator> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::integral_constant<int, 42>>);
+static_assert(std::is_trivially_relocatable_v<std::index_sequence<1,2,3,4>>);
+static_assert(std::is_trivially_relocatable_v<std::ratio<1, 2>>);
+static_assert(std::is_trivially_relocatable_v<std::chrono::system_clock::duration>);
+static_assert(std::is_trivially_relocatable_v<std::chrono::system_clock::time_point>);
+static_assert(std::is_trivially_relocatable_v<std::streampos>);
+static_assert(std::is_trivially_relocatable_v<std::bitset<42>>);
+static_assert(std::is_trivially_relocatable_v<std::bitset<420>>);
+static_assert(!std::is_trivially_relocatable_v<std::unique_lock<std::mutex>>);
+static_assert(!std::is_trivially_relocatable_v<std::shared_lock<SM>>);
+static_assert(!std::is_trivially_relocatable_v<std::thread>);
+static_assert(std::is_trivially_relocatable_v<std::thread::id>);
+static_assert(!std::is_trivially_relocatable_v<std::promise<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::promise<T&>>);
+static_assert(!std::is_trivially_relocatable_v<std::promise<void>>);
+static_assert(!std::is_trivially_relocatable_v<std::future<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::future<T&>>);
+static_assert(!std::is_trivially_relocatable_v<std::future<void>>);
+static_assert(!std::is_trivially_relocatable_v<std::shared_future<T>>);
+static_assert(!std::is_trivially_relocatable_v<std::shared_future<T&>>);
+static_assert(!std::is_trivially_relocatable_v<std::shared_future<void>>);
+static_assert(!std::is_trivially_relocatable_v<std::function<void()>>);  // contains pointer into self
+static_assert(!std::is_trivially_relocatable_v<std::packaged_task<T(T)>>);  // contains pointer into self
+static_assert(!std::is_trivially_relocatable_v<std::packaged_task<T&(T)>>);  // contains pointer into self
+static_assert(!std::is_trivially_relocatable_v<std::packaged_task<void(T)>>);  // contains pointer into self
+
+// The following types are not move-constructible at all.
+static_assert(!std::is_trivially_relocatable_v<std::atomic<int>>);
+static_assert(!std::is_trivially_relocatable_v<std::atomic_flag>);
+static_assert(!std::is_trivially_relocatable_v<std::condition_variable>);
+static_assert(!std::is_trivially_relocatable_v<std::condition_variable_any>);
+static_assert(!std::is_trivially_relocatable_v<std::lock_guard<std::mutex>>);
+static_assert(!std::is_trivially_relocatable_v<std::mutex>);
+static_assert(!std::is_trivially_relocatable_v<std::scoped_lock<std::mutex>>);
+static_assert(!std::is_trivially_relocatable_v<std::seed_seq>);
+static_assert(!std::is_trivially_relocatable_v<std::shared_mutex>);
+
+#if TEST_STD_VER >= 20
+static_assert(!std::is_trivially_relocatable_v<std::barrier<>>);
+static_assert(!std::is_trivially_relocatable_v<std::binary_semaphore>);
+static_assert(!std::is_trivially_relocatable_v<std::counting_semaphore<3>>);
+//static_assert(std::is_trivially_relocatable_v<std::jthread>);
+static_assert(!std::is_trivially_relocatable_v<std::latch>);
+static_assert(std::is_trivially_relocatable_v<std::partial_ordering>);
+static_assert(std::is_trivially_relocatable_v<std::ranges::subrange<T*>>);
+static_assert(std::is_trivially_relocatable_v<std::ranges::subrange<std::string::iterator>> == NotDebug);
+static_assert(std::is_trivially_relocatable_v<std::source_location>);
+static_assert(std::is_trivially_relocatable_v<std::span<T>>);
+//static_assert(std::is_trivially_relocatable_v<std::stop_source>);
+//static_assert(std::is_trivially_relocatable_v<std::stop_token>);
+static_assert(std::is_trivially_relocatable_v<std::strong_ordering>);
+static_assert(std::is_trivially_relocatable_v<std::weak_ordering>);
+#endif
+
+#if TEST_STD_VER >= 23
+static_assert(!std::is_trivially_relocatable_v<std::move_only_function<int()>>);
+static_assert(!std::is_trivially_relocatable_v<std::move_only_function<void()>>);
+//static_assert(std::is_trivially_relocatable_v<std::flat_map<K,T>>);
+//static_assert(std::is_trivially_relocatable_v<std::flat_map<K,bool>>);
+//static_assert(std::is_trivially_relocatable_v<std::flat_multimap<K,T>>);
+//static_assert(std::is_trivially_relocatable_v<std::flat_multimap<K,bool>>);
+//static_assert(std::is_trivially_relocatable_v<std::flat_multiset<K>>);
+//static_assert(std::is_trivially_relocatable_v<std::flat_multiset<bool>>);
+//static_assert(std::is_trivially_relocatable_v<std::flat_set<K>>);
+//static_assert(std::is_trivially_relocatable_v<std::flat_set<bool>>);
+#endif
