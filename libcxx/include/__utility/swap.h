cd15abfe2928409e451eafa7367aaabdcaab34a2
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2019-11-16 15:04:56 -0500

    [libc++] Trivially relocatable (3c/3): Optimize std::swap for trivially relocatable types.
    There are at least two caveats here:
    
    - We must use `memcpy`, not `relocate_at`, to copy bytes to and from
      our deliberately underaligned stack array. We cannot use
      `memcpy` (nor `relocate_at`) in constexpr-land,
      so we use `__libcpp_is_constant_evaluated` to detect that.
    
    - Swapping "potentially overlapping subobjects" can swap too many
      bytes. This is already a problem for `std::copy`, though.

--- libcxx/include/__utility/swap.h
+++ libcxx/include/__utility/swap.h
@@ -10,11 +10,16 @@
 #define _LIBCPP___UTILITY_SWAP_H
 
 #include <__config>
+#include <__memory/addressof.h>
+#include <__type_traits/is_constant_evaluated.h>
+#include <__type_traits/is_empty.h>
 #include <__type_traits/is_move_assignable.h>
 #include <__type_traits/is_move_constructible.h>
 #include <__type_traits/is_nothrow_move_assignable.h>
 #include <__type_traits/is_nothrow_move_constructible.h>
 #include <__type_traits/is_swappable.h>
+#include <__type_traits/is_trivially_relocatable.h>
+#include <__type_traits/is_volatile.h>
 #include <__utility/declval.h>
 #include <__utility/move.h>
 #include <cstddef>
@@ -35,10 +40,26 @@ using __swap_result_t = void;
 
 template <class _Tp>
 inline _LIBCPP_INLINE_VISIBILITY __swap_result_t<_Tp> _LIBCPP_CONSTEXPR_SINCE_CXX20 swap(_Tp& __x, _Tp& __y)
-    _NOEXCEPT_(is_nothrow_move_constructible<_Tp>::value&& is_nothrow_move_assignable<_Tp>::value) {
-  _Tp __t(_VSTD::move(__x));
-  __x = _VSTD::move(__y);
-  __y = _VSTD::move(__t);
+    _NOEXCEPT_(is_nothrow_move_constructible<_Tp>::value && is_nothrow_move_assignable<_Tp>::value)
+{
+    if (__libcpp_is_trivially_relocatable<_Tp>::value && !is_volatile<_Tp>::value && !__libcpp_is_constant_evaluated()) {
+        if (!is_empty<_Tp>::value) {
+            // TODO: if either __x or __y is a "possibly overlapping subobject"
+            // in Itanium ABI terms, this will memcpy too much data. But the
+            // same caveat has applied to std::copy and friends, since forever.
+            char __t[sizeof (_Tp)];
+            // We cannot use __libcpp_relocate_at here, because the stack array
+            // is deliberately underaligned for an actual object of type _Tp.
+            // We are deliberately shuffling bytes here, not objects.
+            ::__builtin_memcpy(__t, std::addressof(__x), sizeof(_Tp));
+            ::__builtin_memmove(std::addressof(__x), std::addressof(__y), sizeof(_Tp));
+            ::__builtin_memcpy(std::addressof(__y), __t, sizeof(_Tp));
+        }
+    } else {
+        _Tp __t(std::move(__x));
+        __x = std::move(__y);
+        __y = std::move(__t);
+    }
 }
 
 template <class _Tp, size_t _Np>
