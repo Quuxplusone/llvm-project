01875610b1afe9826cbfc47e59e4fb0da78e6e89
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2019-11-16 15:03:01 -0500

    Trivially relocatable (2/3): Add explicit warrants to std library types.
    All this stuff uses the name `__libcpp_is_trivially_relocatable`
    instead of `std::is_trivially_relocatable`.

---
+++ libcxx/test/libcxx/containers/sequences/vector/trivially_relocatable.pass.cpp
@@ -0,0 +1,159 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03
+
+// <vector>
+
+// template<class T> class vector;
+
+#include <memory>
+#include <vector>
+#include <type_traits>
+
+// Define the P1144 name for convenience.
+#define is_trivially_relocatable __libcpp_is_trivially_relocatable
+
+struct Incomplete;
+struct NonTriv { ~NonTriv(); };
+
+namespace fa {
+    template<class T>
+    struct FakeAllocator : std::allocator<T> {
+        template<class U> struct rebind {
+            using other = FakeAllocator<U>;
+        };
+        template<class U>
+        FakeAllocator(const FakeAllocator<U>&);
+        // The defaulted (trivial) copy constructor is still present here!
+    };
+    static_assert(std::is_trivially_copy_constructible<FakeAllocator<int>>::value, "");
+    static_assert(std::is_trivially_relocatable<FakeAllocator<int>>::value, "");
+
+    template<class T> using vector = std::vector<T, FakeAllocator<T>>;
+}
+
+namespace fa2 {
+    template<class T>
+    struct FakeAllocator : std::allocator<T> {
+        template<class U> struct rebind {
+            using other = FakeAllocator<U>;
+        };
+        template<class U>
+        FakeAllocator(const FakeAllocator<U>&);
+        FakeAllocator(const FakeAllocator&);
+    };
+    static_assert(!std::is_trivially_copy_constructible<FakeAllocator<int>>::value, "");
+    static_assert(!std::is_trivially_relocatable<FakeAllocator<int>>::value, "");
+
+    template<class T> using vector = std::vector<T, FakeAllocator<T>>;
+}
+
+namespace fa3 {
+    template<class T>
+    struct FakePointer {
+        int offset;
+        FakePointer();
+        FakePointer(const FakePointer&) = default;
+        FakePointer& operator=(const FakePointer&) = default;
+        T& operator*() const;
+        T* operator->() const;
+        FakePointer operator + (unsigned int) const;
+        FakePointer& operator += (unsigned int);
+        FakePointer operator ++(int);
+        FakePointer& operator ++();
+        FakePointer operator --(int);
+        FakePointer& operator --();
+    };
+    static_assert(std::is_trivially_copy_constructible<FakePointer<int>>::value, "");
+    static_assert(std::is_trivially_relocatable<FakePointer<int>>::value, "");
+
+    template<class T>
+    struct FakeAllocator : std::allocator<T> {
+        template<class U> struct rebind {
+            using other = FakeAllocator<U>;
+        };
+        using pointer = FakePointer<T>;
+    };
+
+    template<class T> using vector = std::vector<T, FakeAllocator<T>>;
+}
+
+namespace fa4 {
+    template<class T>
+    struct FakePointer {
+        int offset;
+        FakePointer();
+        FakePointer(const FakePointer&);
+        FakePointer& operator=(const FakePointer&);
+        T& operator*() const;
+        T* operator->() const;
+        FakePointer operator + (unsigned int) const;
+        FakePointer& operator += (unsigned int);
+        FakePointer operator ++(int);
+        FakePointer& operator ++();
+        FakePointer operator --(int);
+        FakePointer& operator --();
+    };
+    static_assert(!std::is_trivially_copy_constructible<FakePointer<int>>::value, "");
+    static_assert(!std::is_trivially_relocatable<FakePointer<int>>::value, "");
+
+    template<class T>
+    struct FakeAllocator : std::allocator<T> {
+        template<class U> struct rebind {
+            using other = FakeAllocator<U>;
+        };
+        using pointer = FakePointer<T>;
+    };
+
+    template<class T> using vector = std::vector<T, FakeAllocator<T>>;
+}
+
+
+static_assert(std::is_trivially_relocatable< std::allocator<int> >::value, "");
+static_assert(!std::is_trivially_relocatable< fa2::FakeAllocator<int> >::value, "");
+static_assert(std::is_trivially_relocatable< fa4::FakeAllocator<int> >::value, "");
+static_assert(!std::is_trivially_relocatable< fa4::FakePointer<int> >::value, "");
+
+template<class VT, bool Expected>
+void check_trivial_relocatability()
+{
+#if _LIBCPP_DEBUG_LEVEL >= 2
+    // Debugging adds significant operations to vector's move-constructor and destructor.
+    static_assert(std::is_trivially_relocatable< VT >::value == false, "");
+#elif !__has_extension(trivially_relocatable)
+    // Making vector trivially relocatable requires using the attribute.
+    static_assert(std::is_trivially_relocatable< VT >::value == false, "");
+#else
+    static_assert(std::is_trivially_relocatable< VT >::value == Expected, "");
+#endif
+}
+
+int main()
+{
+    check_trivial_relocatability< std::vector<int>, true >();
+    check_trivial_relocatability< std::vector<Incomplete>, true >();
+    check_trivial_relocatability< std::vector<NonTriv>, true >();
+
+    check_trivial_relocatability< fa::vector<int>, true >();
+    check_trivial_relocatability< fa::vector<Incomplete>, true >();
+    check_trivial_relocatability< fa::vector<NonTriv>, true >();
+
+    check_trivial_relocatability< fa2::vector<int>, false >();
+    check_trivial_relocatability< fa2::vector<Incomplete>, false >();
+    check_trivial_relocatability< fa2::vector<NonTriv>, false >();
+
+    check_trivial_relocatability< fa3::vector<int>, true >();
+    check_trivial_relocatability< fa3::vector<Incomplete>, true >();
+    check_trivial_relocatability< fa3::vector<NonTriv>, true >();
+
+    check_trivial_relocatability< fa4::vector<int>, false >();
+    check_trivial_relocatability< fa4::vector<Incomplete>, false >();
+    check_trivial_relocatability< fa4::vector<NonTriv>, false >();
+}
---
+++ libcxx/test/libcxx/containers/unord/unord.set/trivially_relocatable.pass.cpp
@@ -0,0 +1,179 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03
+
+// <unordered_set>
+
+#include <unordered_set>
+#include <memory>
+#include <type_traits>
+
+// Define the P1144 name for convenience.
+#define is_trivially_relocatable __libcpp_is_trivially_relocatable
+
+
+struct NonTriv { ~NonTriv(); };
+template <> struct std::hash<NonTriv> { size_t operator()(const NonTriv&) const; };
+bool operator==(const NonTriv&, const NonTriv&);
+
+namespace fa {
+    template<class T>
+    struct FakeAllocator : std::allocator<T> {
+        template<class U> struct rebind {
+            using other = FakeAllocator<U>;
+        };
+        template<class U>
+        FakeAllocator(const FakeAllocator<U>&);
+        // The defaulted (trivial) copy constructor is still present here!
+    };
+    static_assert(std::is_trivially_copy_constructible<FakeAllocator<int>>::value, "");
+    static_assert(std::is_trivially_relocatable<FakeAllocator<int>>::value, "");
+
+    template<class T> using unordered_set = std::unordered_set<T, std::hash<T>, std::equal_to<T>, FakeAllocator<T>>;
+}
+
+namespace fa2 {
+    template<class T>
+    struct FakeAllocator : std::allocator<T> {
+        template<class U> struct rebind {
+            using other = FakeAllocator<U>;
+        };
+        template<class U>
+        FakeAllocator(const FakeAllocator<U>&);
+        FakeAllocator(const FakeAllocator&);
+    };
+    static_assert(!std::is_trivially_copy_constructible<FakeAllocator<int>>::value, "");
+    static_assert(!std::is_trivially_relocatable<FakeAllocator<int>>::value, "");
+
+    template<class T> using unordered_set = std::unordered_set<T, std::hash<T>, std::equal_to<T>, FakeAllocator<T>>;
+}
+
+namespace fa3 {
+    template<class T>
+    struct FakePointer {
+        int offset;
+        FakePointer();
+        FakePointer(const FakePointer&) = default;
+        FakePointer& operator=(const FakePointer&) = default;
+        std::conditional_t<std::is_void<T>::value, char, T>& operator*() const;
+        T* operator->() const;
+        FakePointer operator + (unsigned int) const;
+        FakePointer& operator += (unsigned int);
+        FakePointer operator ++(int);
+        FakePointer& operator ++();
+        FakePointer operator --(int);
+        FakePointer& operator --();
+    };
+    static_assert(std::is_trivially_copy_constructible<FakePointer<int>>::value, "");
+    static_assert(std::is_trivially_relocatable<FakePointer<int>>::value, "");
+
+    template<class T>
+    struct FakeAllocator : std::allocator<T> {
+        template<class U> struct rebind {
+            using other = FakeAllocator<U>;
+        };
+        using pointer = FakePointer<T>;
+    };
+
+    template<class T> using unordered_set = std::unordered_set<T, std::hash<T>, std::equal_to<T>, FakeAllocator<T>>;
+}
+
+namespace fa4 {
+    template<class T>
+    struct FakePointer {
+        int offset;
+        FakePointer();
+        FakePointer(const FakePointer&);
+        FakePointer& operator=(const FakePointer&);
+        std::conditional_t<std::is_void<T>::value, char, T>& operator*() const;
+        T* operator->() const;
+        FakePointer operator + (unsigned int) const;
+        FakePointer& operator += (unsigned int);
+        FakePointer operator ++(int);
+        FakePointer& operator ++();
+        FakePointer operator --(int);
+        FakePointer& operator --();
+    };
+    static_assert(!std::is_trivially_copy_constructible<FakePointer<int>>::value, "");
+    static_assert(!std::is_trivially_relocatable<FakePointer<int>>::value, "");
+
+    template<class T>
+    struct FakeAllocator : std::allocator<T> {
+        template<class U> struct rebind {
+            using other = FakeAllocator<U>;
+        };
+        using pointer = FakePointer<T>;
+    };
+
+    template<class T> using unordered_set = std::unordered_set<T, std::hash<T>, std::equal_to<T>, FakeAllocator<T>>;
+}
+
+namespace fa5 {
+    template<class T>
+    struct FakeHasher : std::hash<T> {
+        ~FakeHasher();
+    };
+    static_assert(!std::is_trivially_relocatable<FakeHasher<int>>::value, "");
+
+    template<class T> using unordered_set = std::unordered_set<T, FakeHasher<T>, std::equal_to<T>, std::allocator<T>>;
+}
+
+namespace fa6 {
+    template<class T>
+    struct FakeEqualTo : std::equal_to<T> {
+        ~FakeEqualTo();
+    };
+    static_assert(!std::is_trivially_relocatable<FakeEqualTo<int>>::value, "");
+
+    template<class T> using unordered_set = std::unordered_set<T, std::hash<T>, FakeEqualTo<T>, std::allocator<T>>;
+}
+
+static_assert(std::is_trivially_relocatable< std::allocator<int> >::value, "");
+static_assert(!std::is_trivially_relocatable< fa2::FakeAllocator<int> >::value, "");
+static_assert(std::is_trivially_relocatable< fa4::FakeAllocator<int> >::value, "");
+static_assert(!std::is_trivially_relocatable< fa4::FakePointer<int> >::value, "");
+
+template<class VT, bool Expected>
+void check_trivial_relocatability()
+{
+#if _LIBCPP_DEBUG_LEVEL >= 2
+    // Debugging adds significant operations to unordered_set's move-constructor and destructor.
+    static_assert(std::is_trivially_relocatable< VT >::value == false, "");
+#elif !__has_extension(trivially_relocatable)
+    // Making unordered_set trivially relocatable requires using the attribute.
+    static_assert(std::is_trivially_relocatable< VT >::value == false, "");
+#else
+    static_assert(std::is_trivially_relocatable< VT >::value == Expected, "");
+#endif
+}
+
+int main()
+{
+    check_trivial_relocatability< std::unordered_set<int>, true >();
+    check_trivial_relocatability< std::unordered_set<NonTriv>, true >();
+
+    check_trivial_relocatability< fa::unordered_set<int>, true >();
+    check_trivial_relocatability< fa::unordered_set<NonTriv>, true >();
+
+    check_trivial_relocatability< fa2::unordered_set<int>, false >();
+    check_trivial_relocatability< fa2::unordered_set<NonTriv>, false >();
+
+    check_trivial_relocatability< fa3::unordered_set<int>, true >();
+    check_trivial_relocatability< fa3::unordered_set<NonTriv>, true >();
+
+    check_trivial_relocatability< fa4::unordered_set<int>, false >();
+    check_trivial_relocatability< fa4::unordered_set<NonTriv>, false >();
+
+    check_trivial_relocatability< fa5::unordered_set<int>, false >();
+    check_trivial_relocatability< fa5::unordered_set<NonTriv>, false >();
+
+    check_trivial_relocatability< fa6::unordered_set<int>, false >();
+    check_trivial_relocatability< fa6::unordered_set<NonTriv>, false >();
+}
