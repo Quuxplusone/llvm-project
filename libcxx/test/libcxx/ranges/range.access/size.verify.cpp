e541f085a3da2ef72fa27ebb19757b7a258a04c0
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-03-21 01:31:52 -0400

    [libc++] Better SFINAE failure diagnostics for CPOs

--- libcxx/test/libcxx/ranges/range.access/size.verify.cpp
+++ libcxx/test/libcxx/ranges/range.access/size.verify.cpp
@@ -16,33 +16,20 @@
 void f() {
   std::ranges::size(42);
   // expected-error@-1 {{no matching function for call}}
-  // expected-note@*:* {{candidate template ignored: could not match}}
-  // expected-note@*:* {{candidate template ignored: could not match}}
   // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'int' does not satisfy '__member_size'}}
-  // expected-note@*:* {{because 'int' does not satisfy '__workaround_52970'}}
-  // expected-note@*:* {{because 'is_class_v<__remove_cvref_t<int> >' evaluated to false}}
-  // expected-note@*:* {{and 'is_union_v<__remove_cvref_t<int> >' evaluated to false}}
-  // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'int' does not satisfy '__unqualified_size'}}
-  // expected-note@*:* {{because 'remove_cvref_t<int>' (aka 'int') does not satisfy '__class_or_enum'}}
-  // expected-note@*:* {{because 'is_class_v<int>' evaluated to false}}
-  // expected-note@*:* {{and 'is_union_v<int>' evaluated to false}}
-  // expected-note@*:* {{and 'is_enum_v<int>' evaluated to false}}
-  // expected-note@*:* {{candidate template ignored: substitution failure}}
+  // expected-note@*:* {{because 'is_bounded_array_v<remove_reference_t<int> >' evaluated to false}}
+  // expected-note@*:* {{and 'remove_reference_t<int>' (aka 'int') does not satisfy '__class_or_enum'}}
+  // expected-note@*:* {{because '_Bool(is_class_v<int> || is_union_v<int> || is_enum_v<int>)' evaluated to false}}
+  // expected-note@*:* {{and 'ranges::end(__t) - ranges::begin(__t)' would be invalid}}
 }
 
 void g() {
   struct S {} s;
   std::ranges::size(s);
   // expected-error@-1 {{no matching function for call}}
-  // expected-note@*:* {{candidate template ignored: could not match}}
-  // expected-note@*:* {{candidate template ignored: could not match}}
   // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'S &' does not satisfy '__member_size'}}
-  // expected-note@*:* {{because 'static_cast<typename decay<decltype((__t.size()))>::type>(__t.size())' would be invalid}}
-  // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'S &' does not satisfy '__unqualified_size'}}
-  // expected-note@*:* {{because 'static_cast<typename decay<decltype((size(__t)))>::type>(size(__t))' would be invalid}}
-  // expected-note@*:* {{candidate template ignored: substitution failure}}
+  // expected-note@*:* {{because 'is_bounded_array_v<remove_reference_t<S &> >' evaluated to false}}
+  // expected-note@*:* {{and '__t.size()' would be invalid}}
+  // expected-note@*:* {{and 'size(__t)' would be invalid}}
+  // expected-note@*:* {{and 'ranges::end(__t) - ranges::begin(__t)' would be invalid}}
 }
