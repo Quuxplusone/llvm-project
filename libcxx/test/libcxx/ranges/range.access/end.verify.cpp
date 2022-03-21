a9c82645079b2fabbc80a9e8fe3d69fcdd473485
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-03-21 01:31:52 -0400

    [libc++] Better SFINAE failure diagnostics for CPOs

--- libcxx/test/libcxx/ranges/range.access/end.verify.cpp
+++ libcxx/test/libcxx/ranges/range.access/end.verify.cpp
@@ -16,72 +16,38 @@
 
 void nonclasstype() {
   std::ranges::end(42);
-  // expected-error@-1 {{call to deleted function call operator}}
-  // expected-note@*:* {{candidate function [with auto:1 = int] has been explicitly deleted}}
-  // expected-note@*:* {{candidate template ignored: could not match}}
+  // expected-error@-1 {{no matching function for call}}
   // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'int' does not satisfy '__member_end'}}
-  // expected-note@*:* {{because 'int' does not satisfy '__can_borrow'}}
-  // expected-note@*:* {{because 'is_lvalue_reference_v<int>' evaluated to false}}
-  // expected-note@*:* {{and 'enable_borrowed_range<remove_cvref_t<int> >' evaluated to false}}
-  // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'int' does not satisfy '__unqualified_end'}}
-  // expected-note@*:* {{because 'int' does not satisfy '__can_borrow'}}
-  // expected-note@*:* {{because 'is_lvalue_reference_v<int>' evaluated to false}}
-  // expected-note@*:* {{and 'enable_borrowed_range<remove_cvref_t<int> >' evaluated to false}}
-  // expected-error@-14 {{attempt to use a deleted function}}
-  // expected-note@*:* {{'operator()<int>' has been explicitly marked deleted here}}
+  // expected-note@*:* {{because 'remove_cvref_t<int>' (aka 'int') does not satisfy '__class_or_enum'}}
+  // expected-note@*:* {{because '_Bool(is_class_v<int> || is_union_v<int> || is_enum_v<int>)' evaluated to false}}
+  // expected-note@*:* {{and 'is_bounded_array_v<remove_reference_t<int> >' evaluated to false}}
 }
 
 void classtype() {
   struct S {} s;
   std::ranges::end(s);
-  // expected-error@-1 {{call to deleted function call operator}}
-  // expected-note@*:* {{candidate function [with auto:1 = S &] has been explicitly deleted}}
-  // expected-note@*:* {{candidate template ignored: could not match}}
+  // expected-error@-1 {{no matching function for call}}
   // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'S &' does not satisfy '__member_end'}}
-  // expected-note@*:* {{because 'typename iterator_t<_Tp>' would be invalid}}
-  // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'S &' does not satisfy '__unqualified_end'}}
-  // expected-note@*:* {{because 'typename iterator_t<_Tp>' would be invalid}}
-  // expected-error@-10 {{attempt to use a deleted function}}
-  // expected-note@*:* {{'operator()<S &>' has been explicitly marked deleted here}}
+  // expected-note@*:* {{because '__t.end()' would be invalid}}
+  // expected-note@*:* {{and 'end(__t)' would be invalid}}
+  // expected-note@*:* {{and 'is_bounded_array_v<remove_reference_t<S &> >' evaluated to false}}
 }
 
 void baditerator() {
   struct S { int *begin(); int end(); } s;
   std::ranges::end(s);
-  // expected-error@-1 {{call to deleted function call operator}}
-  // expected-note@*:* {{candidate function [with auto:1 = S &] has been explicitly deleted}}
-  // expected-note@*:* {{candidate template ignored: could not match}}
+  // expected-error@-1 {{no matching function for call}}
   // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'S &' does not satisfy '__member_end'}}
-  // expected-note@*:* {{because type constraint 'sentinel_for<int, iterator_t<S &> >' was not satisfied}}
+  // expected-note@*:* {{because type constraint 'sentinel_for<int, iterator_t<remove_reference_t<S &> > >' was not satisfied}}
   // expected-note@*:* {{because '__weakly_equality_comparable_with<int, int *>' evaluated to false}}
   // expected-note@*:* {{because '__t == __u' would be invalid}}
-  // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'S &' does not satisfy '__unqualified_end'}}
-  // expected-note@*:* {{because 'static_cast<typename decay<decltype((end(__t)))>::type>(end(__t))' would be invalid}}
-  // expected-error@-12 {{attempt to use a deleted function}}
-  // expected-note@*:* {{'operator()<S &>' has been explicitly marked deleted here}}
+  // expected-note@*:* {{and 'end(__t)' would be invalid}}
+  // expected-note@*:* {{and 'is_bounded_array_v<remove_reference_t<S &> >' evaluated to false}}
 }
 
 void rvalue() {
   std::ranges::end(std::string());
-  // expected-error@-1 {{call to deleted function call operator}}
-  // expected-note@*:* {{candidate function [with auto:1 = std::string] has been explicitly deleted}}
-  // expected-note@*:* {{candidate template ignored: could not match}}
+  // expected-error@-1 {{no matching function for call}}
   // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'std::string' does not satisfy '__member_end'}}
-  // expected-note@*:* {{because 'std::string' does not satisfy '__can_borrow'}}
-  // expected-note@*:* {{because 'is_lvalue_reference_v<std::string>' evaluated to false}}
-  // expected-note@*:* {{and 'enable_borrowed_range<remove_cvref_t<string> >' evaluated to false}}
-  // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'std::string' does not satisfy '__unqualified_end'}}
-  // expected-note@*:* {{because 'std::string' does not satisfy '__can_borrow'}}
-  // expected-note@*:* {{because 'is_lvalue_reference_v<std::string>' evaluated to false}}
-  // expected-note@*:* {{and 'enable_borrowed_range<remove_cvref_t<string> >' evaluated to false}}
-  // expected-error@-14 {{attempt to use a deleted function}}
-  // expected-note@*:* {{'operator()<std::string>' has been explicitly marked deleted here}}
+  // expected-note@*:* {{because '__go(std::forward<_Tp>(__t), __priority_tag<4>())' would be invalid}}
 }
