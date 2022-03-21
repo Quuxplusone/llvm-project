44a8f20e1d14f589275e760e5c6fcf3a68ef0dd9
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-03-21 01:31:52 -0400

    [libc++] Better SFINAE failure diagnostics for CPOs

--- libcxx/test/libcxx/ranges/range.access/data.verify.cpp
+++ libcxx/test/libcxx/ranges/range.access/data.verify.cpp
@@ -17,15 +17,9 @@ void f() {
   std::ranges::data(42);
   // expected-error@-1 {{no matching function for call}}
   // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'int' does not satisfy '__member_data'}}
-  // expected-note@*:* {{because 'int' does not satisfy '__can_borrow'}}
-  // expected-note@*:* {{because 'is_lvalue_reference_v<int>' evaluated to false}}
-  // expected-note@*:* {{and 'enable_borrowed_range<remove_cvref_t<int> >' evaluated to false}}
-  // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'int' does not satisfy '__ranges_begin_invocable'}}
-  // expected-note@*:* {{because 'int' does not satisfy '__can_borrow'}}
-  // expected-note@*:* {{because 'is_lvalue_reference_v<int>' evaluated to false}}
-  // expected-note@*:* {{and 'enable_borrowed_range<remove_cvref_t<int> >' evaluated to false}}
+  // expected-note@*:* {{because 'remove_reference_t<int>' (aka 'int') does not satisfy '__class_or_enum'}}
+  // expected-note@*:* {{because '_Bool(is_class_v<int> || is_union_v<int> || is_enum_v<int>)' evaluated to false}}
+  // expected-note@*:* {{and 'ranges::begin(__t)' would be invalid}}
 }
 
 void g() {
@@ -33,9 +27,6 @@ void g() {
   std::ranges::data(s);
   // expected-error@-1 {{no matching function for call}}
   // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'S &' does not satisfy '__member_data'}}
-  // expected-note@*:* {{because 'static_cast<typename decay<decltype((__t.data()))>::type>(__t.data())' would be invalid}}
-  // expected-note@*:* {{candidate template ignored: constraints not satisfied}}
-  // expected-note@*:* {{because 'S &' does not satisfy '__ranges_begin_invocable'}}
-  // expected-note@*:* {{because 'ranges::begin(__t)' would be invalid}}
+  // expected-note@*:* {{because '__t.data()' would be invalid}}
+  // expected-note@*:* {{and 'ranges::begin(__t)' would be invalid}}
 }
