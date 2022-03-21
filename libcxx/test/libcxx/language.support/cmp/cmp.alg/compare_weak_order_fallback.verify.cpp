ce49d321d4fbe97a95ac8818c04ec3fa52ed1ea1
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-03-20 23:35:34 -0400

    New tests for SFINAE failure diagnostics on CPOs

---
+++ libcxx/test/libcxx/language.support/cmp/cmp.alg/compare_weak_order_fallback.verify.cpp
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+
+// std::compare_weak_order_fallback
+//   Substitution failure should give reasonably nice diagnostics.
+
+#include <compare>
+
+void different_types() {
+  std::compare_weak_order_fallback(42, 42L);
+  // expected-error@-1 {{no matching function for call}}
+  // expected-note@*:* {{candidate template ignored: substitution failure}}
+}
+
+void no_operators() {
+  struct S {} s;
+  std::compare_weak_order_fallback(s, s);
+  // expected-error@-1 {{no matching function for call}}
+  // expected-note@*:* {{candidate template ignored: substitution failure}}
+}
+
+void not_enough_operators() {
+  struct S {
+    bool operator==(const S&) const { return false; }
+  } s;
+  std::compare_weak_order_fallback(s, s);
+  // expected-error@-1 {{no matching function for call}}
+  // expected-note@*:* {{candidate template ignored: substitution failure}}
+}
