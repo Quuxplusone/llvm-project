573b5de5b46e3689af494d9d1feb347f4d1d2ce2
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-07-31 12:06:51 -0400

    [libc++] Fix a tuple-recursion-depth issue.
    Reported on Slack, 2022-07-31:
    https://cpplang.slack.com/archives/C2X659D1B/p1659280990052819

--- libcxx/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/recursion_depth.pass.cpp
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/recursion_depth.pass.cpp
@@ -15,20 +15,36 @@
 #include <cassert>
 #include <utility>
 
+#include "test_macros.h"
+
 template <std::size_t... I>
-constexpr void CreateTuple(std::index_sequence<I...>) {
+constexpr bool CreateTuple(std::index_sequence<I...>) {
   std::tuple<decltype(I)...> tuple(I...);
   assert(std::get<0>(tuple) == 0);
   assert(std::get<sizeof...(I)-1>(tuple) == sizeof...(I)-1);
+  return true;
 }
 
-constexpr bool test() {
-  CreateTuple(std::make_index_sequence<1024>{});
+template <std::size_t... I>
+TEST_CONSTEXPR_CXX20 bool AssignTuple(std::index_sequence<I...>) {
+  std::tuple<decltype(I)...> tuple(I...);
+  auto t2 = tuple;             // copy constructor
+  auto t3 = std::move(tuple);  // move constructor
+  decltype(tuple) t4;          // default constructor
+  t3 = t2;                     // copy assignment
+  t4 = std::move(t2);          // move assignment
+  swap(t3, t4);                // swap
+  // equality (t3 == t4) does not work
   return true;
 }
 
 int main(int, char**) {
-  test();
-  static_assert(test(), "");
+  CreateTuple(std::make_index_sequence<1024>{});
+  static_assert(CreateTuple(std::make_index_sequence<1024>{}), "");
+  AssignTuple(std::make_index_sequence<1024>{});
+#if TEST_STD_VER >= 20
+  static_assert(AssignTuple(std::make_index_sequence<1024>{}));
+#endif
+
   return 0;
 }
