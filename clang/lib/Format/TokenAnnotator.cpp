41f7a522f31f7c84632ce4069951f004dcb174b7
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-01-06 21:32:34 -0500

    [clang] Implement #embed (WG14 N3017, WG21 N1967)
    Original work by Jean-Heyd Meneide, CC0.
    Implements parameter suggestions from Hubert Tong.
    Maintenance and bugfixes by Arthur O'Dwyer.
    
    Originally squash-cherry-picked from https://github.com/ThePhD/llvm-project
    branch "dang", commits:
    (cherry picked from commit 8f92f539b793b58c54427e53264015f65534ad68)
    (cherry picked from commit a323907e7d61cc65dbb783031b5ab940981bc15f)
    (cherry picked from commit 6c0079371087007779ff0b5ed2b6c75548542b32)
    (cherry picked from commit f0d083104db592ec1ced40dbf1f929c73ded7cde)
    (cherry picked from commit 191e59e4af25a13d4e11dbfe6da8718a34faefae)
    (cherry picked from commit 62ed501b24e7cf71845c2bd805237b56303d5e3a)
    
    Some tests were cribbed from
    https://github.com/ThePhD/embed/tree/5413c81/examples

--- clang/lib/Format/TokenAnnotator.cpp
+++ clang/lib/Format/TokenAnnotator.cpp
@@ -1413,7 +1413,8 @@ private:
       break;
     case tok::identifier:
       if (Tok->isOneOf(Keywords.kw___has_include,
-                       Keywords.kw___has_include_next)) {
+                       Keywords.kw___has_include_next,
+                       Keywords.kw___has_embed)) {
         parseHasInclude();
       }
       if (Style.isCSharp() && Tok->is(Keywords.kw_where) && Tok->Next &&
@@ -1549,6 +1550,7 @@ private:
     case tok::pp_include:
     case tok::pp_include_next:
     case tok::pp_import:
+    case tok::pp_embed:
       next();
       parseIncludeDirective();
       Type = LT_ImportStatement;
@@ -1575,7 +1577,8 @@ private:
       if (Tok->is(tok::l_paren)) {
         parseParens();
       } else if (Tok->isOneOf(Keywords.kw___has_include,
-                              Keywords.kw___has_include_next)) {
+                              Keywords.kw___has_include_next,
+                              Keywords.kw___has_embed)) {
         parseHasInclude();
       }
     }
