8888275c83c0503e2ce129577ba86c220bc0800d
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-01-17 17:13:58 -0500

    [libc++] [P1222] Implement std::flat_set
    Includes the fixes for LWG3786, LWG3884, LWG3802, LWG3803, LWG3804.
    See my "flat-set" branch for detailed commit messages.

--- libcxx/utils/libcxx/test/modules.py
+++ libcxx/utils/libcxx/test/modules.py
@@ -92,8 +92,9 @@ ExtraHeader["ranges"] = "v1/__fwd/subrange.h$"
 # same definition.
 ExtraHeader["functional"] = "v1/__compare/compare_three_way.h$"
 
-# <flat_set> reuses some functionality defined inside <flat_map>
-ExtraHeader["flat_set"] = "v1/__flat_map/sorted_.+.h$"
+# <flat_set> and <flat_map> reuse some shared functionality
+for header in ("flat_set",):
+    ExtraHeader[header] = "v1/__utility/sorted_.*.h$"
 
 # Some C compatibility headers define std::size_t, which is in <__cstddef/size_t.h>
 for header in ("cstdio", "cstdlib", "cstring", "ctime", "cuchar", "cwchar"):
