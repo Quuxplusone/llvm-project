f4071858de4953e5c99d84187f783bd5f5529af7
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-01-31 16:08:17 -0500

    [libc++] [P0447] [P2596] Implement std::hive
    Cribbed from https://github.com/Quuxplusone/SG14/ .
    This implements only the P2596 API, since I like it better.
    We also implement `.push_back` and `.emplace_back`, since they're cheap.

---
+++ libcxx/test/std/containers/sequences/hive/empty.verify.cpp
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <hive>
+
+// bool empty() const noexcept;
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+
+#include <hive>
+
+void test() {
+    std::hive<int> c;
+    c.empty(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+}
---
+++ libcxx/test/std/containers/sequences/hive/reverse_iterators.pass.cpp
@@ -0,0 +1,79 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+
+// <hive>
+
+// reverse_iterator       rbegin();
+// reverse_iterator       rend();
+// const_reverse_iterator rbegin()  const;
+// const_reverse_iterator rend()    const;
+// const_reverse_iterator crbegin() const;
+// const_reverse_iterator crend()   const;
+
+#include <hive>
+#include <cassert>
+#include <iterator>
+
+#include "min_allocator.h"
+
+template <class Vector>
+void check_hive_reverse_iterators() {
+    {
+        Vector vec;
+        assert(vec.rbegin() == vec.rend());
+        assert(vec.crbegin() == vec.crend());
+    }
+    {
+        const int n = 10;
+        Vector vec;
+        const Vector& cvec = vec;
+        vec.reserve(n);
+        for (int i = 0; i < n; ++i) {
+            vec.insert(i);
+        }
+        vec.sort();
+        {
+            int iterations = 0;
+
+            for (typename Vector::const_reverse_iterator it = vec.crbegin(); it != vec.crend(); ++it) {
+                assert(*it == (n - iterations - 1));
+                ++iterations;
+            }
+            assert(iterations == n);
+        }
+        {
+            assert(cvec.rbegin() == vec.crbegin());
+            assert(cvec.rend() == vec.crend());
+        }
+        {
+            int iterations = 0;
+
+            for (typename Vector::reverse_iterator it = vec.rbegin(); it != vec.rend(); ++it) {
+                assert(*it == (n - iterations - 1));
+                *it = 40;
+                assert(*it == 40);
+                ++iterations;
+            }
+            assert(iterations == n);
+        }
+
+        assert(std::distance(vec.rbegin(), vec.rend()) == n);
+        assert(std::distance(cvec.rbegin(), cvec.rend()) == n);
+        assert(std::distance(vec.crbegin(), vec.crend()) == n);
+        assert(std::distance(cvec.crbegin(), cvec.crend()) == n);
+    }
+}
+
+int main(int, char**) {
+    check_hive_reverse_iterators<std::hive<int>>();
+    check_hive_reverse_iterators<std::hive<int, min_allocator<int>>>();
+
+    return 0;
+}
