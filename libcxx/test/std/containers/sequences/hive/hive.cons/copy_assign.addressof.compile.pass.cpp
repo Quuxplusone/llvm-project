fb81e25f2cc4300343ab93a3e25f5f5e665f4ab7
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-01-31 16:08:17 -0500

    [libc++] [P0447] [P2596] Implement std::hive
    Cribbed from https://github.com/Quuxplusone/SG14/ .
    This implements only the P2596 API, since I like it better.
    We also implement `.push_back` and `.emplace_back`, since they're cheap.
    
    Support C++17, C++20, and C++23.

---
+++ libcxx/test/std/containers/sequences/hive/hive.cons/copy_assign.addressof.compile.pass.cpp
@@ -0,0 +1,26 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+
+// <hive>
+
+// hive& operator=(const hive& c);
+
+// Validate whether the container can be copy-assigned with an ADL-hijacking operator&
+
+#include <hive>
+
+#include "test_macros.h"
+#include "operator_hijacker.h"
+
+void test() {
+  std::hive<operator_hijacker> lo;
+  std::hive<operator_hijacker> l;
+  l = lo;
+}
