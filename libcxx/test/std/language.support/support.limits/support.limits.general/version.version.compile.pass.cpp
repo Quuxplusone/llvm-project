d88445edd6d186d376d8d66de197cde30cb1515f
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-01-31 16:08:17 -0500

    [libc++] [P0447] [P2596] Implement std::hive
    Cribbed from https://github.com/Quuxplusone/SG14/ .
    This implements only the P2596 API, since I like it better.
    We also implement `.push_back` and `.emplace_back`, since they're cheap.
    
    Support C++17, C++20, and C++23.

--- libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
+++ libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
@@ -87,6 +87,7 @@
     __cpp_lib_generic_unordered_lookup             201811L [C++20]
     __cpp_lib_hardware_interference_size           201703L [C++17]
     __cpp_lib_has_unique_object_representations    201606L [C++17]
+    __cpp_lib_hive                                 202300L [C++20]
     __cpp_lib_hypot                                201603L [C++17]
     __cpp_lib_incomplete_container_elements        201505L [C++17]
     __cpp_lib_int_pow2                             202002L [C++20]
@@ -464,6 +465,10 @@
 
 # ifdef __cpp_lib_has_unique_object_representations
 #   error "__cpp_lib_has_unique_object_representations should not be defined before c++17"
+# endif
+
+# ifdef __cpp_lib_hive
+#   error "__cpp_lib_hive should not be defined before c++20"
 # endif
 
 # ifdef __cpp_lib_hypot
@@ -1134,6 +1139,10 @@
 
 # ifdef __cpp_lib_has_unique_object_representations
 #   error "__cpp_lib_has_unique_object_representations should not be defined before c++17"
+# endif
+
+# ifdef __cpp_lib_hive
+#   error "__cpp_lib_hive should not be defined before c++20"
 # endif
 
 # ifdef __cpp_lib_hypot
@@ -1930,6 +1939,10 @@
 # endif
 # if __cpp_lib_has_unique_object_representations != 201606L
 #   error "__cpp_lib_has_unique_object_representations should have the value 201606L in c++17"
+# endif
+
+# ifdef __cpp_lib_hive
+#   error "__cpp_lib_hive should not be defined before c++20"
 # endif
 
 # ifndef __cpp_lib_hypot
@@ -2993,6 +3006,13 @@
 # endif
 # if __cpp_lib_has_unique_object_representations != 201606L
 #   error "__cpp_lib_has_unique_object_representations should have the value 201606L in c++20"
+# endif
+
+# ifndef __cpp_lib_hive
+#   error "__cpp_lib_hive should be defined in c++20"
+# endif
+# if __cpp_lib_hive != 202300L
+#   error "__cpp_lib_hive should have the value 202300L in c++20"
 # endif
 
 # ifndef __cpp_lib_hypot
@@ -4257,6 +4277,13 @@
 # endif
 # if __cpp_lib_has_unique_object_representations != 201606L
 #   error "__cpp_lib_has_unique_object_representations should have the value 201606L in c++23"
+# endif
+
+# ifndef __cpp_lib_hive
+#   error "__cpp_lib_hive should be defined in c++23"
+# endif
+# if __cpp_lib_hive != 202300L
+#   error "__cpp_lib_hive should have the value 202300L in c++23"
 # endif
 
 # ifndef __cpp_lib_hypot
@@ -5665,6 +5692,13 @@
 # endif
 # if __cpp_lib_has_unique_object_representations != 201606L
 #   error "__cpp_lib_has_unique_object_representations should have the value 201606L in c++26"
+# endif
+
+# ifndef __cpp_lib_hive
+#   error "__cpp_lib_hive should be defined in c++26"
+# endif
+# if __cpp_lib_hive != 202300L
+#   error "__cpp_lib_hive should have the value 202300L in c++26"
 # endif
 
 # ifndef __cpp_lib_hypot
