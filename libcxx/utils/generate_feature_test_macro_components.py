c5ff9f4ea7a3d92dfc6924c1e720542d777d3033
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-01-31 16:08:17 -0500

    [libc++] [P0447] [P2596] Implement std::hive
    Cribbed from https://github.com/Quuxplusone/SG14/ .
    This implements only the P2596 API, since I like it better.
    We also implement `.push_back` and `.emplace_back`, since they're cheap.
    
    Support C++17, C++20, and C++23.

--- libcxx/utils/generate_feature_test_macro_components.py
+++ libcxx/utils/generate_feature_test_macro_components.py
@@ -529,6 +529,11 @@ feature_test_macros = [
             "unimplemented": True,
         },
         {
+            "name": "__cpp_lib_hive",
+            "values": { "c++20": 202300 },
+            "headers": ["hive"],
+        },
+        {
             "name": "__cpp_lib_hypot",
             "values": {"c++17": 201603},
             "headers": ["cmath"],
