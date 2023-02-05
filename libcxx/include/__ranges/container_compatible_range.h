03addb76e80c9912176f9675902c5a15640376f8
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-02-05 13:41:45 -0500

    [libc++] [P1206] Add `container-compatible-range` and the `from_range` tag type
    This is just enough of P1206 to support adding the new constructors and
    methods to new and/or existing containers piecemeal. No existing container
    has yet been touched.
    
    Differential Revision: https://reviews.llvm.org/D142335

---
+++ libcxx/include/__ranges/container_compatible_range.h
@@ -0,0 +1,32 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCPP___RANGES_CONTAINER_COMPATIBLE_RANGE_H
+#define _LIBCPP___RANGES_CONTAINER_COMPATIBLE_RANGE_H
+
+#include <__concepts/convertible_to.h>
+#include <__config>
+#include <__ranges/concepts.h>
+
+#if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
+#  pragma GCC system_header
+#endif
+
+_LIBCPP_BEGIN_NAMESPACE_STD
+
+#if _LIBCPP_STD_VER >= 23
+
+template <class _Rp, class _Tp>
+concept __container_compatible_range = ranges::input_range<_Rp> && convertible_to<ranges::range_reference_t<_Rp>, _Tp>;
+
+#endif // _LIBCPP_STD_VER >= 23
+
+_LIBCPP_END_NAMESPACE_STD
+
+#endif // _LIBCPP___RANGES_CONTAINER_COMPATIBLE_RANGE_H
