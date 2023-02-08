6f7cd3b86d357de16919e5fdc25924cc9bbad313
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-02-08 13:50:47 -0500

    [libc++] Use stable sorting in flat_set insertion
    This conforming extension makes `flat_set` behave more like `set`,
    which is a good thing. The new "*_stability.pass.cpp" tests
    all fail before this commit and pass after it.

---
+++ libcxx/test/std/containers/container.adaptors/flat.multiset/insert_iter_iter_stability.pass.cpp
@@ -0,0 +1,50 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// <flat_set>
+
+// template <class InputIterator>
+//   void insert(InputIterator first, InputIterator last);
+//
+// libc++ uses stable_sort to ensure that flat_multiset's behavior matches multiset's,
+// in terms of which duplicate items are kept.
+// This tests a conforming extension.
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <flat_set>
+#include <random>
+#include <set>
+
+#include "test_macros.h"
+
+struct Mod256 {
+  bool operator()(int x, int y) const { return (x % 256) < (y % 256); }
+};
+
+int main(int, char**)
+{
+  {
+    std::mt19937 randomness;
+    uint16_t values[400];
+    std::generate(values, values + 400, randomness);
+
+    std::multiset<uint16_t, Mod256> s(values, values + 200);
+    std::flat_multiset<uint16_t, Mod256> fs(std::sorted_equivalent, s.begin(), s.end());
+    assert(std::ranges::equal(fs, s));
+
+    fs.insert(values + 200, values + 400);
+    s.insert(values + 200, values + 400);
+    assert(fs.size() == s.size());
+    LIBCPP_ASSERT(std::ranges::equal(fs, s));
+  }
+  return 0;
+}
