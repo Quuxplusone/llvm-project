8f5f1ae19ffefdcf6fa76e22b57ac0c5c3e2d27a
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-02-28 14:25:04 -0500

    [libc++] Implement `poke_heap` and `priority_queue::replace_top`
    https://quuxplusone.github.io/blog/2018/04/27/pq-replace-top/
    https://github.com/Quuxplusone/libcxx/commit/pq-replace-top
    
    Lacks tests.

--- libcxx/test/libcxx/algorithms/ranges_robust_against_copying_comparators.pass.cpp
+++ libcxx/test/libcxx/algorithms/ranges_robust_against_copying_comparators.pass.cpp
@@ -184,6 +184,8 @@ constexpr bool all_the_algorithms()
     (void)std::ranges::partition_copy(a, first2, last2, UnaryTrue(&copies)); assert(copies == 0);
     (void)std::ranges::partition_point(first, last, UnaryTrue(&copies)); assert(copies == 0);
     (void)std::ranges::partition_point(a, UnaryTrue(&copies)); assert(copies == 0);
+    (void)std::ranges::poke_heap(first, last, Less(&copies)); assert(copies == 0);
+    (void)std::ranges::poke_heap(a, Less(&copies)); assert(copies == 0);
     (void)std::ranges::pop_heap(first, last, Less(&copies)); assert(copies == 0);
     (void)std::ranges::pop_heap(a, Less(&copies)); assert(copies == 0);
     (void)std::ranges::prev_permutation(first, last, Less(&copies)); assert(copies == 0);
