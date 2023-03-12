fa35b1293edb79724c5be1dececfa32e419e9660
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-03-12 14:13:03 -0400

    [libc++] Trivially relocatable (2b/3): Add explicit warrants to std::flat_{multi,}{set,map}

--- libcxx/include/__flat_set/flat_set.h
+++ libcxx/include/__flat_set/flat_set.h
@@ -265,6 +265,7 @@ namespace std {
 #include <__type_traits/is_nothrow_assignable.h>
 #include <__type_traits/is_nothrow_constructible.h>
 #include <__type_traits/is_same.h>
+#include <__type_traits/is_trivially_relocatable.h>
 #include <__type_traits/type_identity.h>
 #include <__utility/exception_guard.h>
 #include <__utility/forward.h>
@@ -318,7 +319,10 @@ __flatset_unique(_FwdIt __first, _FwdIt __last, reference_wrapper<const _Compare
 
 template <class _Key, class _Compare = less<_Key>,
           class _KeyContainer = vector<_Key>>
-class flat_set {
+class _LIBCPP_TRIVIALLY_RELOCATABLE_IF(
+  __libcpp_is_trivially_relocatable<_KeyContainer>::value &&
+  __libcpp_is_trivially_relocatable<_Compare>::value
+) flat_set {
   static_assert(is_same_v<_Key, typename _KeyContainer::value_type>);
   static_assert(__has_random_access_iterator_category<typename _KeyContainer::iterator>::value);
   static_assert(__has_random_access_iterator_category<typename _KeyContainer::const_iterator>::value);
