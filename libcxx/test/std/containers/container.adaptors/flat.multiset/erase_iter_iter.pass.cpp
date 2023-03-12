308d3d38844911cfbbed101719a4f9bc078ff009
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-03-12 11:47:13 -0400

    [libc++] [P1222] Implement std::flat_multiset

---
+++ libcxx/test/std/containers/container.adaptors/flat.multiset/erase_iter_iter.pass.cpp
@@ -0,0 +1,70 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// <flat_set>
+
+// iterator erase(const_iterator first, const_iterator last);
+
+#include <compare>
+#include <concepts>
+#include <deque>
+#include <flat_set>
+#include <functional>
+#include <utility>
+
+#include "test_macros.h"
+#include "min_allocator.h"
+
+int main(int, char**)
+{
+  {
+    using M = std::flat_multiset<int>;
+    M m = {1,2,3,5,5,6,7,8};
+    ASSERT_SAME_TYPE(decltype(m.erase(m.cend(), m.end())), M::iterator);
+    M::iterator it = m.erase(m.cbegin() + 5, m.cbegin() + 5);
+    assert(it == m.cbegin() + 5);
+    assert((m == M{1,2,3,5,5,6,7,8}));
+    it = m.erase(m.cbegin() + 3, m.begin() + 4);
+    assert(it == m.begin() + 3);
+    assert((m == M{1,2,3,5,6,7,8}));
+    it = m.erase(m.begin() + 2, m.cbegin() + 5);
+    assert(it == m.begin() + 2);
+    assert((m == M{1,2,7,8}));
+    it = m.erase(m.begin(), it);
+    assert(it == m.begin());
+    assert((m == M{7,8}));
+    it = m.erase(m.begin(), m.end());
+    assert(it == m.end());
+    assert(m.empty());
+  }
+  {
+    using M = std::flat_multiset<int, std::less<>, std::deque<int, min_allocator<int>>>;
+    M::container_type container = {5,6,7,8};
+    container.insert(container.begin(), {1,3,3,4});
+    M m = M(std::move(container));
+    ASSERT_SAME_TYPE(decltype(m.erase(m.cend(), m.end())), M::iterator);
+    M::iterator it = m.erase(m.cbegin() + 5, m.cbegin() + 5);
+    assert(it == m.cbegin() + 5);
+    assert((m == M{1,3,3,4,5,6,7,8}));
+    it = m.erase(m.cbegin() + 3, m.begin() + 4);
+    assert(it == m.begin() + 3);
+    assert((m == M{1,3,3,5,6,7,8}));
+    it = m.erase(m.begin() + 2, m.cbegin() + 5);
+    assert(it == m.begin() + 2);
+    assert((m == M{1,3,7,8}));
+    it = m.erase(m.begin(), it);
+    assert(it == m.begin());
+    assert((m == M{7,8}));
+    it = m.erase(m.begin(), m.end());
+    assert(it == m.end());
+    assert(m.empty());
+  }
+  return 0;
+}
---
+++ libcxx/test/std/containers/container.adaptors/flat.multiset/transparent_element_construction.pass.cpp
@@ -0,0 +1,297 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// <flat_set>
+
+// Make sure each "transparent comparator" member function
+// constructs the actual value_type at the appropriate point
+// for that specific member function (if ever).
+
+#include <cassert>
+#include <deque>
+#include <flat_set>
+#include <functional>
+
+#include "test_macros.h"
+
+struct Counter {
+  // Unlike in "Counter.h", these counts never go back down.
+  Counter(int i) : i_(i) { gConstructed += 1; }
+  Counter(Counter&& rhs) noexcept : i_(rhs.i_) { if (i_ == 4) gMoveConstructed += 1; }
+  Counter(const Counter& rhs) : i_(rhs.i_) { if (i_ == 4) gCopyConstructed += 1; }
+  Counter& operator=(const Counter&) = default;
+  Counter& operator=(Counter&&) = default;
+  ~Counter() = default;
+  auto operator<=>(const Counter&) const = default;
+  auto operator<=>(int i) const { return i_ <=> i; }
+  bool operator==(int i) const { return i_ == i; }
+  int i_;
+
+  static int gConstructed;
+  static int gMoveConstructed;
+  static int gCopyConstructed;
+
+  static void reset() {
+    gConstructed = 0;
+    gMoveConstructed = 0;
+    gCopyConstructed = 0;
+  }
+};
+int Counter::gConstructed = 0;
+int Counter::gMoveConstructed = 0;
+int Counter::gCopyConstructed = 0;
+
+void test_insert()
+{
+  // Inserting at the beginning or end of M won't move any existing elements.
+  using M = std::flat_multiset<Counter, std::less<>, std::deque<Counter>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    m.insert(Counter(4));
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 1);
+    assert(Counter::gCopyConstructed == 0);
+  }
+  {
+    M m = {1, 2, 3};
+    Counter lvalue = 4;
+    Counter::reset();
+    m.insert(lvalue);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 1);
+  }
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    m.insert(4);
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 1); // [P2767] flat_multiset lacks transparent insert(K&&)
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+void test_insert_hint()
+{
+  using M = std::flat_multiset<Counter, std::less<>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    m.insert(m.begin(), Counter(4));
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 1);
+    assert(Counter::gCopyConstructed == 0);
+  }
+  {
+    M m = {1, 2, 3};
+    Counter lvalue = 4;
+    Counter::reset();
+    m.insert(m.begin(), lvalue);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 1);
+  }
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    m.insert(m.begin(), 4);
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 1); // [P2767] flat_multiset lacks transparent insert(K&&)
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+void test_emplace()
+{
+  using M = std::flat_multiset<Counter, std::less<>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    m.emplace(4);
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 1);
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+void test_emplace_hint()
+{
+  using M = std::flat_multiset<Counter, std::less<>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    m.emplace_hint(m.begin(), 4);
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 1);
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+void test_contains()
+{
+  using M = std::flat_multiset<Counter, std::less<>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    (void)m.contains(Counter(4));
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter::reset();
+    (void)m.contains(4);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter lvalue = 4;
+    Counter::reset();
+    (void)m.contains(lvalue);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+void test_count()
+{
+  using M = std::flat_multiset<Counter, std::less<>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    (void)m.count(Counter(4));
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter::reset();
+    (void)m.count(4);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter lvalue = 4;
+    Counter::reset();
+    (void)m.count(lvalue);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+void test_find()
+{
+  using M = std::flat_multiset<Counter, std::less<>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    (void)m.find(Counter(4));
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter::reset();
+    (void)m.find(4);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter lvalue = 4;
+    Counter::reset();
+    (void)m.find(lvalue);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+void test_lower_bound()
+{
+  using M = std::flat_multiset<Counter, std::less<>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    (void)m.lower_bound(Counter(4));
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter::reset();
+    (void)m.lower_bound(4);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter lvalue = 4;
+    Counter::reset();
+    (void)m.lower_bound(lvalue);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+void test_upper_bound()
+{
+  using M = std::flat_multiset<Counter, std::less<>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    (void)m.upper_bound(Counter(4));
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter::reset();
+    (void)m.upper_bound(4);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter lvalue = 4;
+    Counter::reset();
+    (void)m.upper_bound(lvalue);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+void test_equal_range()
+{
+  using M = std::flat_multiset<Counter, std::less<>>;
+  {
+    M m = {1, 2, 3};
+    Counter::reset();
+    (void)m.equal_range(Counter(4));
+    assert(Counter::gConstructed == 1);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter::reset();
+    (void)m.equal_range(4);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+    Counter lvalue = 4;
+    Counter::reset();
+    (void)m.equal_range(lvalue);
+    assert(Counter::gConstructed == 0);
+    assert(Counter::gMoveConstructed == 0);
+    assert(Counter::gCopyConstructed == 0);
+  }
+}
+
+int main(int, char**)
+{
+  test_insert();
+  test_insert_hint();
+  test_emplace();
+  test_emplace_hint();
+
+  test_contains();
+  test_count();
+  test_find();
+  test_lower_bound();
+  test_upper_bound();
+  test_equal_range();
+
+  return 0;
+}
