5cf17398ff7bc1bd6e4dc15f4fbf88d5b80d335f
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-03-14 11:29:00 -0400

    [libc++] [test] Reformat deduct.pass.cpp for sequence containers
    Also add a test for `vector(42, allocator<int>())` since it doesn't
    do what you'd naturally think it does.

--- libcxx/test/std/containers/sequences/deque/deque.cons/deduct.pass.cpp
+++ libcxx/test/std/containers/sequences/deque/deque.cons/deduct.pass.cpp
@@ -6,8 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-// <deque>
 // UNSUPPORTED: c++03, c++11, c++14
+
+// <deque>
 
 // template <class InputIterator, class Allocator = allocator<typename iterator_traits<InputIterator>::value_type>>
 //    deque(InputIterator, InputIterator, Allocator = Allocator())
@@ -17,14 +18,11 @@
 //   deque(from_range_t, R&&, Allocator = Allocator())
 //     -> deque<ranges::range_value_t<R>, Allocator>; // C++23
 
-#include "asan_testing.h"
-#include <array>
 #include <cassert>
 #include <climits> // INT_MAX
-#include <cstddef>
 #include <deque>
-#include <iterator>
 
+#include "asan_testing.h"
 #include "deduction_guides_sfinae_checks.h"
 #include "test_macros.h"
 #include "test_iterators.h"
@@ -32,132 +30,119 @@
 
 struct A {};
 
-int main(int, char**)
-{
-
-//  Test the explicit deduction guides
-    {
-    const int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    std::deque deq(std::begin(arr), std::end(arr));
-
-    static_assert(std::is_same_v<decltype(deq), std::deque<int>>, "");
+bool tests() {
+  {
+    const int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::deque deq(arr, arr + 10);
+    ASSERT_SAME_TYPE(decltype(deq), std::deque<int>);
     assert(std::equal(deq.begin(), deq.end(), std::begin(arr), std::end(arr)));
     LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-    }
-
-    {
-    const long arr[] = {INT_MAX, 1L, 2L, 3L };
-    std::deque deq(std::begin(arr), std::end(arr), std::allocator<long>());
-    static_assert(std::is_same_v<decltype(deq)::value_type, long>, "");
-    assert(deq.size() == 4);
-    assert(deq[0] == INT_MAX);
-    assert(deq[1] == 1L);
-    assert(deq[2] == 2L);
+  }
+  {
+    const long arr[] = { INT_MAX, 1L, 2L, 3L };
+    std::deque deq(arr, arr + 4, std::allocator<long>());
+    ASSERT_SAME_TYPE(decltype(deq), std::deque<long>);
+    assert((deq == std::deque<long>{INT_MAX,1,2,3}));
     LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-    }
-
-//  Test the implicit deduction guides
-
-    {
-//  We don't expect this one to work.
-//  std::deque deq(std::allocator<int>()); // deque (allocator &)
-    }
-
-    {
-    std::deque deq(1, A{}); // deque (size_type, T)
-    static_assert(std::is_same_v<decltype(deq)::value_type, A>, "");
-    static_assert(std::is_same_v<decltype(deq)::allocator_type, std::allocator<A>>, "");
-    assert(deq.size() == 1);
+  }
+  {
+    std::deque deq(42, A{}); // (Count, T)
+    ASSERT_SAME_TYPE(decltype(deq), std::deque<A>);
+    assert(deq.size() == 42);
     LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-    }
-
-    {
-    std::deque deq(1, A{}, test_allocator<A>()); // deque (size_type, T, allocator)
-    static_assert(std::is_same_v<decltype(deq)::value_type, A>, "");
-    static_assert(std::is_same_v<decltype(deq)::allocator_type, test_allocator<A>>, "");
-    assert(deq.size() == 1);
+  }
+  {
+    std::deque deq(42, std::allocator<int>()); // (Count, T) again!
+    ASSERT_SAME_TYPE(decltype(deq), std::deque<std::allocator<int>>);
+    assert(deq.size() == 42);
     LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-    }
-
-    {
-    std::deque deq{1U, 2U, 3U, 4U, 5U}; // deque(initializer-list)
-    static_assert(std::is_same_v<decltype(deq)::value_type, unsigned>, "");
-    assert(deq.size() == 5);
-    assert(deq[2] == 3U);
+  }
+  {
+    std::deque deq(42, A{}, test_allocator<A>(0, 43)); // (Count, T, Alloc)
+    ASSERT_SAME_TYPE(decltype(deq), std::deque<A, test_allocator<A>>);
+    assert(deq.size() == 42);
+    assert(deq.get_allocator().get_id() == 43);
     LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-    }
-
-    {
-    std::deque deq({1.0, 2.0, 3.0, 4.0}, test_allocator<double>()); // deque(initializer-list, allocator)
-    static_assert(std::is_same_v<decltype(deq)::value_type, double>, "");
-    static_assert(std::is_same_v<decltype(deq)::allocator_type, test_allocator<double>>, "");
-    assert(deq.size() == 4);
-    assert(deq[3] == 4.0);
+  }
+  {
+    std::deque deq = {1u, 2u, 3u, 4u, 5u}; // (initializer-list)
+    ASSERT_SAME_TYPE(decltype(deq), std::deque<unsigned>);
+    assert((deq == std::deque<unsigned>{1,2,3,4,5}));
     LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-    }
-
-    {
+  }
+  {
+    std::deque deq({1.0, 2.0, 3.0, 4.0}, test_allocator<double>(0, 42)); // (initializer-list, Alloc)
+    ASSERT_SAME_TYPE(decltype(deq), std::deque<double, test_allocator<double>>);
+    assert((deq == decltype(deq){1,2,3,4}));
+    assert(deq.get_allocator().get_id() == 42);
+    LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
+  }
+  {
     std::deque<long double> source;
-    std::deque deq(source); // deque(deque &)
-    static_assert(std::is_same_v<decltype(deq)::value_type, long double>, "");
-    static_assert(std::is_same_v<decltype(deq)::allocator_type, std::allocator<long double>>, "");
+    std::deque deq(source); // copy ctor
+    ASSERT_SAME_TYPE(decltype(deq), std::deque<long double>);
     assert(deq.size() == 0);
     LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-    }
-
+    LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(source));
+  }
+  {
+    typedef test_allocator<short> Alloc;
+    typedef test_allocator<int> ConvertibleToAlloc;
     {
-        typedef test_allocator<short> Alloc;
-        typedef test_allocator<int> ConvertibleToAlloc;
-
-        {
-        std::deque<short, Alloc> source;
-        std::deque deq(source, Alloc(2));
-        static_assert(std::is_same_v<decltype(deq), decltype(source)>);
-        LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-        LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(source));
-        }
-
-        {
-        std::deque<short, Alloc> source;
-        std::deque deq(source, ConvertibleToAlloc(2));
-        static_assert(std::is_same_v<decltype(deq), decltype(source)>);
-        LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-        LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(source));
-        }
-
-        {
-        std::deque<short, Alloc> source;
-        std::deque deq(std::move(source), Alloc(2));
-        static_assert(std::is_same_v<decltype(deq), decltype(source)>);
-        LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-        LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(source));
-        }
-
-        {
-        std::deque<short, Alloc> source;
-        std::deque deq(std::move(source), ConvertibleToAlloc(2));
-        static_assert(std::is_same_v<decltype(deq), decltype(source)>);
-        LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
-        LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(source));
-        }
+      std::deque<short, Alloc> source;
+      std::deque deq(source, Alloc(2));
+      static_assert(std::is_same_v<decltype(deq), decltype(source)>);
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(source));
     }
-
+    {
+      std::deque<short, Alloc> source;
+      std::deque deq(source, ConvertibleToAlloc(2));
+      static_assert(std::is_same_v<decltype(deq), decltype(source)>);
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(source));
+    }
+    {
+      std::deque<short, Alloc> source;
+      std::deque deq(std::move(source), Alloc(2));
+      static_assert(std::is_same_v<decltype(deq), decltype(source)>);
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(source));
+    }
+    {
+      std::deque<short, Alloc> source;
+      std::deque deq(std::move(source), ConvertibleToAlloc(2));
+      static_assert(std::is_same_v<decltype(deq), decltype(source)>);
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(source));
+    }
+  }
 #if TEST_STD_VER >= 23
+  {
     {
-      {
-        std::deque c(std::from_range, std::array<int, 0>());
-        static_assert(std::is_same_v<decltype(c), std::deque<int>>);
-      }
-
-      {
-        using Alloc = test_allocator<int>;
-        std::deque c(std::from_range, std::array<int, 0>(), Alloc());
-        static_assert(std::is_same_v<decltype(c), std::deque<int, Alloc>>);
-      }
+      int a[2] = {1,2};
+      std::deque deq(std::from_range, a);
+      ASSERT_SAME_TYPE(decltype(deq), std::deque<int>);
+      assert(deq.size() == 2);
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
     }
+    {
+      using Alloc = test_allocator<int>;
+      int a[2] = {1,2};
+      std::deque deq(std::from_range, a, Alloc());
+      ASSERT_SAME_TYPE(decltype(deq), std::deque<int, Alloc>);
+      assert(deq.size() == 2);
+      LIBCPP_ASSERT(is_double_ended_contiguous_container_asan_correct(deq));
+    }
+  }
 #endif
 
-    SequenceContainerDeductionGuidesSfinaeAway<std::deque, std::deque<int>>();
+  SequenceContainerDeductionGuidesSfinaeAway<std::deque, std::deque<int>>();
 
-    return 0;
+  return true;
+}
+
+int main(int, char**) {
+  tests();
+  return 0;
 }
