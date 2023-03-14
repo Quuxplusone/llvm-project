63fca16250c9a4871b12dca19c85c820397515c6
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-03-14 10:02:01 -0400

    [libc++] [test] Rename deduct.fail.cpp -> deduct.verify.cpp, and reformat
    All tests have been reformatted to 2-space indents and commented
    in the modern style. Remove the repetition of Standardese deduction guides,
    since the important thing about these verify tests is that they reject
    the right things, not that they "exercise" specific signatures (which
    by definition they will not, because the point is that no specific signature
    matches, hence CTAD fails).

---
+++ libcxx/test/std/containers/sequences/array/array.cons/deduct.verify.cpp
@@ -0,0 +1,28 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+
+// <array>
+
+// Test CTAD on cases where deduction should fail.
+
+#include <array>
+
+void test() {
+  {
+    // Cannot deduce T from nothing
+    std::array a;
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'array'}}
+  }
+  {
+    // Cannot deduce T from a mixed initializer list
+    std::array arr{1,2,3L};
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'array'}}
+  }
+}
