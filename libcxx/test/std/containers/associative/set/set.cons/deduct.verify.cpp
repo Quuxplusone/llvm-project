dab77df3641c92a56f39a2d75cc09c6f3f2f96b9
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-03-14 10:02:01 -0400

    [libc++] [test] Rename deduct.fail.cpp -> deduct.verify.cpp, and reformat
    All tests have been reformatted to 2-space indents and commented
    in the modern style. Remove the repetition of Standardese deduction guides,
    since the important thing about these verify tests is that they reject
    the right things, not that they "exercise" specific signatures (which
    by definition they will not, because the point is that no specific signature
    matches, hence CTAD fails).

---
+++ libcxx/test/std/containers/associative/set/set.cons/deduct.verify.cpp
@@ -0,0 +1,51 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+
+// <set>
+
+// Test CTAD on cases where deduction should fail.
+
+#include <set>
+#include <functional>
+#include <memory>
+
+struct NotAnAllocator {
+  friend bool operator<(NotAnAllocator, NotAnAllocator) { return false; }
+};
+
+void test() {
+  {
+    // cannot deduce Key from nothing
+    std::set s;
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'set'}}
+  }
+  {
+    // cannot deduce Key from just (Compare)
+    std::set s(std::less<int>{});
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'set'}}
+  }
+  {
+    // cannot deduce Key from just (Compare, Allocator)
+    std::set s(std::less<int>{}, std::allocator<int>{});
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'set'}}
+  }
+  {
+    // cannot deduce Key from just (Allocator)
+    std::set s(std::allocator<int>{});
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'set'}}
+  }
+  {
+    // since we have parens, not braces, this deliberately does not find the
+    // initializer_list constructor
+    NotAnAllocator a;
+    std::set s(a);
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'set'}}
+  }
+}
