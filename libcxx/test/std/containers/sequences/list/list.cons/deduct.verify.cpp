c51edff74ad880250ab38bea8e99f019ccefd7ff
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-03-14 10:02:01 -0400

    [libc++] [test] Rename deduct.fail.cpp -> deduct.verify.cpp, and reformat
    All tests have been reformatted to 2-space indents and commented
    in the modern style. Remove the repetition of Standardese deduction guides,
    since the important thing about these verify tests is that they reject
    the right things, not that they "exercise" specific signatures (which
    by definition they will not, because the point is that no specific signature
    matches, hence CTAD fails).

---
+++ libcxx/test/std/containers/sequences/list/list.cons/deduct.verify.cpp
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+
+// <list>
+
+// Test CTAD on cases where deduction should fail.
+
+#include <list>
+#include <memory>
+
+void test() {
+  {
+    // Cannot deduce T from nothing
+    std::list lst;
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'list'}}
+  }
+  {
+    // Cannot deduce T from just (Alloc)
+    auto lst = std::list(std::allocator<int>());
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'list'}}
+  }
+  {
+    // Cannot deduce T from just (Count)
+    auto lst = std::list(42);
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'list'}}
+  }
+}
---
+++ libcxx/test/std/containers/sequences/vector/vector.cons/deduct.verify.cpp
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+
+// <vector>
+
+// Test CTAD on cases where deduction should fail.
+
+#include <vector>
+#include <memory>
+
+void test() {
+  {
+    // Cannot deduce T from nothing
+    std::vector vec;
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'vector'}}
+  }
+  {
+    // Cannot deduce T from just (Alloc)
+    auto vec = std::vector(std::allocator<int>());
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'vector'}}
+  }
+  {
+    // Cannot deduce T from just (Count)
+    auto vec = std::vector(42);
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'vector'}}
+  }
+}
