c01dc7e870ab70ecf13bf1159288d4017dcb06f7
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-03-14 10:02:01 -0400

    [libc++] [test] Rename deduct.fail.cpp -> deduct.verify.cpp, and reformat
    All tests have been reformatted to 2-space indents and commented
    in the modern style. Remove the repetition of Standardese deduction guides,
    since the important thing about these verify tests is that they reject
    the right things, not that they "exercise" specific signatures (which
    by definition they will not, because the point is that no specific signature
    matches, hence CTAD fails).

---
+++ libcxx/test/std/containers/container.adaptors/queue/queue.cons/deduct.verify.cpp
@@ -0,0 +1,42 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+
+// <queue>
+
+// Test CTAD on cases where deduction should fail.
+
+#include <queue>
+#include <list>
+#include <memory>
+
+void test()
+{
+  {
+    // Cannot deduce T from nothing
+    std::queue q;
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
+  }
+  {
+    // Cannot deduce T from just (Alloc)
+    auto q = std::queue(std::allocator<int>());
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
+  }
+  {
+    // The '45' is not an allocator
+    auto q = std::queue(std::list<int>{1,2,3}, 45);
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
+  }
+  {
+    // The '45' is not an allocator
+    std::queue<int> source;
+    auto q = std::queue(source, 45);
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
+  }
+}
