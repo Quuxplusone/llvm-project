8f49838969f77bd449d5b841739b097878d1059b
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-03-14 10:02:01 -0400

    [libc++] [test] Rename deduct.fail.cpp -> deduct.verify.cpp, and reformat
    All tests have been reformatted to 2-space indents and commented
    in the modern style. Remove the repetition of Standardese deduction guides,
    since the important thing about these verify tests is that they reject
    the right things, not that they "exercise" specific signatures (which
    by definition they will not, because the point is that no specific signature
    matches, hence CTAD fails).

---
+++ libcxx/test/std/containers/unord/unord.multimap/unord.multimap.cnstr/deduct.verify.cpp
@@ -0,0 +1,61 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+
+// <unordered_map>
+
+// Test CTAD on cases where deduction should fail.
+
+#include <unordered_map>
+#include <functional>
+#include <memory>
+
+void test() {
+  using P = std::pair<const int, int>;
+  {
+    // cannot deduce Key from nothing
+    std::unordered_multimap m;
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'unordered_multimap'}}
+  }
+  {
+    // cannot deduce Key from just (Size)
+    std::unordered_multimap m(42);
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'unordered_multimap'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Hash)
+    std::unordered_multimap m(42, std::hash<int>());
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'unordered_multimap'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Hash, Pred)
+    std::unordered_multimap m(42, std::hash<int>(), std::equal_to<int>());
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'unordered_multimap'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Hash, Pred, Allocator)
+    std::unordered_multimap m(42, std::hash<int>(), std::equal_to<int>(), std::allocator<P>());
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'unordered_multimap'}}
+  }
+  {
+    // cannot deduce Key from just (Allocator)
+    std::unordered_multimap m(std::allocator<P>{});
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'unordered_multimap'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Allocator)
+    std::unordered_multimap m(42, std::allocator<P>());
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'unordered_multimap'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Hash, Allocator)
+    std::unordered_multimap m(42, std::hash<int>(), std::allocator<P>());
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'unordered_multimap'}}
+  }
+}
