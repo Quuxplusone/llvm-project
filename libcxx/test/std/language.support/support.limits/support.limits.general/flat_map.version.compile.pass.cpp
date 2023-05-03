15a7037d746647d1ef62c003c04a115a4dfd7341
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-05-03 14:05:00 -0400

    [libc++] [P0429] Implement std::flat_multimap

--- libcxx/test/std/language.support/support.limits/support.limits.general/flat_map.version.compile.pass.cpp
+++ libcxx/test/std/language.support/support.limits/support.limits.general/flat_map.version.compile.pass.cpp
@@ -44,32 +44,20 @@
 
 #elif TEST_STD_VER == 23
 
-#  if !defined(_LIBCPP_VERSION)
-#    ifndef __cpp_lib_flat_map
-#      error "__cpp_lib_flat_map should be defined in c++23"
-#    endif
-#    if __cpp_lib_flat_map != 202207L
-#      error "__cpp_lib_flat_map should have the value 202207L in c++23"
-#    endif
-#  else
-#    ifdef __cpp_lib_flat_map
-#      error "__cpp_lib_flat_map should not be defined because it is unimplemented in libc++!"
-#    endif
+#  ifndef __cpp_lib_flat_map
+#    error "__cpp_lib_flat_map should be defined in c++23"
+#  endif
+#  if __cpp_lib_flat_map != 202207L
+#    error "__cpp_lib_flat_map should have the value 202207L in c++23"
 #  endif
 
 #elif TEST_STD_VER > 23
 
-#  if !defined(_LIBCPP_VERSION)
-#    ifndef __cpp_lib_flat_map
-#      error "__cpp_lib_flat_map should be defined in c++26"
-#    endif
-#    if __cpp_lib_flat_map != 202207L
-#      error "__cpp_lib_flat_map should have the value 202207L in c++26"
-#    endif
-#  else
-#    ifdef __cpp_lib_flat_map
-#      error "__cpp_lib_flat_map should not be defined because it is unimplemented in libc++!"
-#    endif
+#  ifndef __cpp_lib_flat_map
+#    error "__cpp_lib_flat_map should be defined in c++26"
+#  endif
+#  if __cpp_lib_flat_map != 202207L
+#    error "__cpp_lib_flat_map should have the value 202207L in c++26"
 #  endif
 
 #endif // TEST_STD_VER > 23
