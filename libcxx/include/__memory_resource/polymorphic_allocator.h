d159b363185f4419b26544b6597fa2863b61bacc
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-06-01 13:04:48 -0400

    [libc++] Allocator traits for trivial relocatability.
    `__allocator_pocma_models_relocatable<A>`
      `A`'s propagate-on-container-foo traits are such that `vector<X,A>`
      ought to successfully model the semantic requirements of `std::relocatable`.
      That is, `destroy_at(&x); relocate_at(&y, &x)` should "always" have the same
      observable effects as `x = move(y); destroy_at(&y)`.
      This is not true for PMR containers when `x` and `y` come from different arenas.
      But we need to make a global decision that contributes to the value of
      `is_trivially_relocatable_v<decltype(x)>` regardless of their values.
      When `_LIBCPP_TRIVIALLY_RELOCATABLE_PMR_CONTAINERS`, lie and make this `true`.
    
    `__allocator_has_trivial_move_construct<A,T>`
      `A::construct` does nothing weird for this particular element type `T`.
      When `_LIBCPP_TRIVIALLY_RELOCATABLE_PMR_CONTAINERS`, lie and make this `true`
      for all types `T`.
    
    `__allocator_has_trivial_destroy<A,T>`
      `A::destroy` does nothing weird for this particular element type `T`.

--- libcxx/include/__memory_resource/polymorphic_allocator.h
+++ libcxx/include/__memory_resource/polymorphic_allocator.h
@@ -12,7 +12,9 @@
 #include <__assert>
 #include <__availability>
 #include <__config>
+#include <__memory/allocator_traits.h>
 #include <__memory_resource/memory_resource.h>
+#include <__type_traits/negation.h>
 #include <__utility/exception_guard.h>
 #include <cstddef>
 #include <limits>
@@ -218,6 +220,25 @@ operator!=(const polymorphic_allocator<_Tp>& __lhs, const polymorphic_allocator<
 #  endif
 
 } // namespace pmr
+
+template <class _Tp, class _Up>
+struct __allocator_has_trivial_copy_construct<pmr::polymorphic_allocator<_Tp>, _Up> : _Not<uses_allocator<_Up, pmr::polymorphic_allocator<_Tp>>> {};
+
+#if _LIBCPP_TRIVIALLY_RELOCATABLE_PMR_CONTAINERS
+template <class _Tp, class _Up>
+struct __allocator_has_trivial_move_construct<pmr::polymorphic_allocator<_Tp>, _Up> : true_type {};
+#else
+template <class _Tp, class _Up>
+struct __allocator_has_trivial_move_construct<pmr::polymorphic_allocator<_Tp>, _Up> : _Not<uses_allocator<_Up, pmr::polymorphic_allocator<_Tp>>> {};
+#endif // _LIBCPP_TRIVIALLY_RELOCATABLE_PMR_CONTAINERS
+
+template <class _Tp, class _Up>
+struct __allocator_has_trivial_destroy<pmr::polymorphic_allocator<_Tp>, _Up> : true_type {};
+
+#if _LIBCPP_TRIVIALLY_RELOCATABLE_PMR_CONTAINERS
+template <class _Tp>
+struct __allocator_pocma_models_relocatable<pmr::polymorphic_allocator<_Tp>> : true_type {};
+#endif // _LIBCPP_TRIVIALLY_RELOCATABLE_PMR_CONTAINERS
 
 _LIBCPP_END_NAMESPACE_STD
 
