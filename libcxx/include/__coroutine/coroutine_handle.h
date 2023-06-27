a87dba1384e067c48f675e47f7a6321991e1b414
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-06-26 21:41:04 -0400

    [libc++] Make a few standard types trivially equality comparable
    This allows std::equal and other algorithms to be optimized better.
    
    Patch (mostly) by Nikolas Klauser!
    
    Differential Revision: https://reviews.llvm.org/D150081

--- libcxx/include/__coroutine/coroutine_handle.h
+++ libcxx/include/__coroutine/coroutine_handle.h
@@ -73,6 +73,9 @@ public:
     __builtin_coro_destroy(__handle_);
   }
 
+    // This makes coroutine_handle<> trivially equality comparable
+    _LIBCPP_HIDE_FROM_ABI bool operator==(const coroutine_handle&) const = default;
+
 private:
   _LIBCPP_HIDE_FROM_ABI bool __is_suspended() const {
     // FIXME actually implement a check for if the coro is suspended.
@@ -83,6 +86,9 @@ private:
 };
 
 // [coroutine.handle.compare]
+
+// The template parameter disambiguates this as a worse match than the defaulted coroutine_handle<>::operator==
+template <class = void>
 inline _LIBCPP_HIDE_FROM_ABI constexpr bool operator==(coroutine_handle<> __x, coroutine_handle<> __y) noexcept {
   return __x.address() == __y.address();
 }
@@ -152,6 +158,9 @@ public:
   _LIBCPP_HIDE_FROM_ABI _Promise& promise() const {
     return *static_cast<_Promise*>(__builtin_coro_promise(this->__handle_, alignof(_Promise), false));
   }
+
+    // This makes coroutine_handle<T> trivially equality comparable
+    _LIBCPP_HIDE_FROM_ABI bool operator==(const coroutine_handle&) const = default;
 
 private:
   _LIBCPP_HIDE_FROM_ABI bool __is_suspended() const {
