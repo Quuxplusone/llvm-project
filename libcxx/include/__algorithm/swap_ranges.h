c964f212e8394860307dc5a0d198a321fed2a023
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-08-01 14:30:29 -0400

    [libc++] Use `__libcpp_memswap` in `swap_ranges` when possible
    This follows the same pattern used for `copy`, `copy_backward`, and `move`,
    except that `swap_ranges` requires four parameters instead of three.
    
    The new tests mainly verify that the new codepaths are correctly disabled
    during constant evaluation. When I wrote them, I was worried that the
    trivial codepath might try to form invalid pointer values. But in fact
    I believe that the trivial codepath is actually never entered unless
    the sentinel is an iterator that unwraps to a pointer type; so we should
    be okay in that respect.

--- libcxx/include/__algorithm/swap_ranges.h
+++ libcxx/include/__algorithm/swap_ranges.h
@@ -9,10 +9,16 @@
 #ifndef _LIBCPP___ALGORITHM_SWAP_RANGES_H
 #define _LIBCPP___ALGORITHM_SWAP_RANGES_H
 
+#include <__algorithm/copy_move_common.h>
 #include <__algorithm/iterator_operations.h>
 #include <__algorithm/min.h>
 #include <__config>
+#include <__cstddef/size_t.h>
 #include <__fwd/bit_reference.h>
+#include <__string/constexpr_c_functions.h>
+#include <__type_traits/datasizeof.h>
+#include <__type_traits/enable_if.h>
+#include <__type_traits/is_constant_evaluated.h>
 #include <__utility/move.h>
 #include <__utility/pair.h>
 #include <__utility/swap.h>
@@ -162,53 +168,119 @@ _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 __bit_iterator<_Cr, false> _
   return __result;
 }
 
-// 2+1 iterators: size2 >= size1; used by std::swap_ranges.
-template <class, class _Cl, class _Cr>
-_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<__bit_iterator<_Cl, false>, __bit_iterator<_Cr, false> >
-__swap_ranges(__bit_iterator<_Cl, false> __first1,
-              __bit_iterator<_Cl, false> __last1,
-              __bit_iterator<_Cr, false> __first2) {
-  if (__first1.__ctz_ == __first2.__ctz_)
-    return std::make_pair(__last1, std::__swap_ranges_aligned(__first1, __last1, __first2));
-  return std::make_pair(__last1, std::__swap_ranges_unaligned(__first1, __last1, __first2));
+template <class _AlgPolicy, class _Tp, class _Up>
+_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 void
+__constexpr_memswap(_Tp* __first1, _Up* __first2, __element_count __n) {
+  size_t __count = static_cast<size_t>(__n);
+  if (__libcpp_is_constant_evaluated()) {
+    for (size_t __i = 0; __i != __count; ++__i)
+      _IterOps<_AlgPolicy>::iter_swap(__first1 + __i, __first2 + __i);
+  } else if (__count > 0) {
+    std::__libcpp_memswap(__first1, __first2, (__count - 1) * sizeof(_Tp) + __datasizeof_v<_Tp>);
+  }
 }
 
-// 2+2 iterators: used by std::ranges::swap_ranges.
-template <class _AlgPolicy, class _Cl, class _Cr>
-_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<__bit_iterator<_Cl, false>, __bit_iterator<_Cr, false> >
-__swap_ranges(__bit_iterator<_Cl, false> __first1,
-              __bit_iterator<_Cl, false> __last1,
-              __bit_iterator<_Cr, false> __first2,
-              __bit_iterator<_Cr, false> __last2) {
-  if (__last1 - __first1 < __last2 - __first2)
-    return std::make_pair(__last1, std::__swap_ranges<_AlgPolicy>(__first1, __last1, __first2).second);
-  return std::make_pair(std::__swap_ranges<_AlgPolicy>(__first2, __last2, __first1).second, __last2);
+template <class _AlgPolicy, class _Tp, class _Up>
+_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<_Tp*, _Up*>
+__swap_ranges_trivial_impl(_Tp* __first1, _Tp* __last1, _Up* __first2) {
+  const size_t __n = static_cast<size_t>(__last1 - __first1);
+
+  std::__constexpr_memswap<_AlgPolicy>(__first1, __first2, __element_count(__n));
+
+  return std::make_pair(__last1, __first2 + __n);
 }
 
-// 2+2 iterators: the shorter size will be used.
-template <class _AlgPolicy, class _ForwardIterator1, class _Sentinel1, class _ForwardIterator2, class _Sentinel2>
-_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<_ForwardIterator1, _ForwardIterator2>
-__swap_ranges(_ForwardIterator1 __first1, _Sentinel1 __last1, _ForwardIterator2 __first2, _Sentinel2 __last2) {
-  while (__first1 != __last1 && __first2 != __last2) {
-    _IterOps<_AlgPolicy>::iter_swap(__first1, __first2);
-    ++__first1;
-    ++__first2;
+template <class _AlgPolicy>
+struct __swap_ranges_impl {
+  // 2+2 iterators: the shorter size will be used.
+  template <class _ForwardIterator1, class _Sentinel1, class _ForwardIterator2, class _Sentinel2>
+  _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
+  pair<_ForwardIterator1, _ForwardIterator2>
+  operator()(_ForwardIterator1 __first1, _Sentinel1 __last1, _ForwardIterator2 __first2, _Sentinel2 __last2) const {
+    while (__first1 != __last1 && __first2 != __last2) {
+      _IterOps<_AlgPolicy>::iter_swap(__first1, __first2);
+      ++__first1;
+      ++__first2;
+    }
+    return pair<_ForwardIterator1, _ForwardIterator2>(std::move(__first1), std::move(__first2));
   }
 
-  return pair<_ForwardIterator1, _ForwardIterator2>(std::move(__first1), std::move(__first2));
-}
+  template <class _Cl, class _Cr>
+  _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<__bit_iterator<_Cl, false>, __bit_iterator<_Cr, false> >
+  operator()(__bit_iterator<_Cl, false> __first1,
+             __bit_iterator<_Cl, false> __last1,
+             __bit_iterator<_Cr, false> __first2,
+             __bit_iterator<_Cr, false> __last2) const {
+    if (__last1 - __first1 < __last2 - __first2)
+      return std::make_pair(__last1, (*this)(__first1, __last1, __first2).second);
+    return std::make_pair((*this)(__first2, __last2, __first1).second, __last2);
+  }
 
-// 2+1 iterators: size2 >= size1.
+  // 2+1 iterators: size2 >= size1.
+  template <class _ForwardIterator1, class _Sentinel1, class _ForwardIterator2>
+  _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
+  pair<_ForwardIterator1, _ForwardIterator2>
+  operator()(_ForwardIterator1 __first1, _Sentinel1 __last1, _ForwardIterator2 __first2) const {
+    while (__first1 != __last1) {
+      _IterOps<_AlgPolicy>::iter_swap(__first1, __first2);
+      ++__first1;
+      ++__first2;
+    }
+    return pair<_ForwardIterator1, _ForwardIterator2>(std::move(__first1), std::move(__first2));
+  }
+
+  template <class _Cl, class _Cr>
+  _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<__bit_iterator<_Cl, false>, __bit_iterator<_Cr, false> >
+  operator()(__bit_iterator<_Cl, false> __first1,
+             __bit_iterator<_Cl, false> __last1,
+             __bit_iterator<_Cr, false> __first2) const {
+    if (__first1.__ctz_ == __first2.__ctz_)
+      return std::make_pair(__last1, std::__swap_ranges_aligned(__first1, __last1, __first2));
+    return std::make_pair(__last1, std::__swap_ranges_unaligned(__first1, __last1, __first2));
+  }
+
+  template <class _Tp, class _Up,
+            __enable_if_t<__can_lower_swap_to_memswap<_Tp, _Up>::value, int> = 0>
+  _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<_Tp*, _Up*>
+  operator()(_Tp* __first1, _Tp* __last1, _Up* __first2) const {
+    return std::__swap_ranges_trivial_impl<_AlgPolicy>(__first1, __last1, __first2);
+  }
+
+  template <class _Tp, class _Up,
+            __enable_if_t<__can_lower_swap_to_memswap<_Tp, _Up>::value, int> = 0>
+  _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<_Tp*, _Up*>
+  operator()(_Tp* __first1, _Tp* __last1, _Up* __first2, _Up* __last2) const {
+    auto __n1 = __last1 - __first1;
+    auto __n2 = __last2 - __first2;
+    auto __n = (__n1 < __n2) ? __n1 : __n2;
+    return std::__swap_ranges_trivial_impl<_AlgPolicy>(__first1, __first1 + __n, __first2);
+  }
+};
+
 template <class _AlgPolicy, class _ForwardIterator1, class _Sentinel1, class _ForwardIterator2>
-_LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 pair<_ForwardIterator1, _ForwardIterator2>
+inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
+pair<_ForwardIterator1, _ForwardIterator2>
 __swap_ranges(_ForwardIterator1 __first1, _Sentinel1 __last1, _ForwardIterator2 __first2) {
-  while (__first1 != __last1) {
-    _IterOps<_AlgPolicy>::iter_swap(__first1, __first2);
-    ++__first1;
-    ++__first2;
+  if (__libcpp_is_constant_evaluated()) {
+    return __swap_ranges_impl<_AlgPolicy>()(
+        std::move(__first1), std::move(__last1), std::move(__first2));
+  } else {
+    return std::__copy_move_unwrap_iters<__swap_ranges_impl<_AlgPolicy> >(
+        std::move(__first1), std::move(__last1), std::move(__first2));
   }
+}
 
-  return pair<_ForwardIterator1, _ForwardIterator2>(std::move(__first1), std::move(__first2));
+template <class _AlgPolicy, class _ForwardIterator1, class _Sentinel1, class _ForwardIterator2, class _Sentinel2>
+inline _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20
+pair<_ForwardIterator1, _ForwardIterator2>
+__swap_ranges(_ForwardIterator1 __first1, _Sentinel1 __last1, _ForwardIterator2 __first2, _Sentinel2 __last2) {
+  if (__libcpp_is_constant_evaluated()) {
+    return __swap_ranges_impl<_AlgPolicy>()(
+        std::move(__first1), std::move(__last1), std::move(__first2), std::move(__last2));
+  } else {
+    return std::__copy_move_unwrap_iters<__swap_ranges_impl<_AlgPolicy> >(
+        std::move(__first1), std::move(__last1), std::move(__first2), std::move(__last2));
+  }
 }
 
 template <class _ForwardIterator1, class _ForwardIterator2>
