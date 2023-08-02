0e05c759ba620f9cce270b3117455ca1d4474c2e
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-08-02 01:31:05 -0400

    [libc++] [test] Reformat *.deduct.verify.cpp
    All tests have been reformatted to 2-space indents and commented
    in the modern style. Remove the repetition of Standardese deduction guides,
    since the important thing about these verify tests is that they reject
    the right things, not that they "exercise" specific signatures (which
    by definition they will not, because the point is that no specific signature
    matches, hence CTAD fails).

--- libcxx/test/std/containers/unord/unord.map/unord.map.cnstr/deduct.verify.cpp
+++ libcxx/test/std/containers/unord/unord.map/unord.map.cnstr/deduct.verify.cpp
@@ -10,96 +10,52 @@
 
 // <unordered_map>
 
-// template<class InputIterator,
-//          class Hash = hash<iter-key-type<InputIterator>>,
-//          class Pred = equal_to<iter-key-type<InputIterator>>,
-//          class Allocator = allocator<iter-to-alloc-type<InputIterator>>>
-// unordered_map(InputIterator, InputIterator, typename see below::size_type = see below,
-//               Hash = Hash(), Pred = Pred(), Allocator = Allocator())
-//   -> unordered_map<iter-key-type<InputIterator>, iter-mapped-type<InputIterator>, Hash, Pred,
-//                    Allocator>;
-//
-// template<class Key, class T, class Hash = hash<Key>,
-//          class Pred = equal_to<Key>, class Allocator = allocator<pair<const Key, T>>>
-// unordered_map(initializer_list<pair<Key, T>>,
-//               typename see below::size_type = see below, Hash = Hash(),
-//               Pred = Pred(), Allocator = Allocator())
-//   -> unordered_map<Key, T, Hash, Pred, Allocator>;
-//
-// template<class InputIterator, class Allocator>
-// unordered_map(InputIterator, InputIterator, typename see below::size_type, Allocator)
-//   -> unordered_map<iter-key-type<InputIterator>, iter-mapped-type<InputIterator>,
-//                    hash<iter-key-type<InputIterator>>,
-//                    equal_to<iter-key-type<InputIterator>>, Allocator>;
-//
-// template<class InputIterator, class Allocator>
-// unordered_map(InputIterator, InputIterator, Allocator)
-//   -> unordered_map<iter-key-type<InputIterator>, iter-mapped-type<InputIterator>,
-//                    hash<iter-key-type<InputIterator>>,
-//                    equal_to<iter-key-type<InputIterator>>, Allocator>;
-//
-// template<class InputIterator, class Hash, class Allocator>
-// unordered_map(InputIterator, InputIterator, typename see below::size_type, Hash, Allocator)
-//   -> unordered_map<iter-key-type<InputIterator>, iter-mapped-type<InputIterator>, Hash,
-//                    equal_to<iter-key-type<InputIterator>>, Allocator>;
-//
-// template<class Key, class T, class Allocator>
-// unordered_map(initializer_list<pair<Key, T>>, typename see below::size_type, Allocator)
-//   -> unordered_map<Key, T, hash<Key>, equal_to<Key>, Allocator>;
-//
-// template<class Key, class T, class Allocator>
-// unordered_map(initializer_list<pair<Key, T>>, Allocator)
-//   -> unordered_map<Key, T, hash<Key>, equal_to<Key>, Allocator>;
-//
-// template<class Key, class T, class Hash, class Allocator>
-// unordered_map(initializer_list<pair<Key, T>>, typename see below::size_type, Hash,
-//               Allocator)
-//   -> unordered_map<Key, T, Hash, equal_to<Key>, Allocator>;
+// Test CTAD on cases where deduction should fail.
 
-#include <functional>
 #include <unordered_map>
+#include <functional>
+#include <memory>
 
-int main(int, char**)
-{
-    using P = std::pair<const int, int>;
-    {
-        // cannot deduce Key from nothing
-        std::unordered_map m; // expected-error-re{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
-    }
-    {
-        // cannot deduce Key from just (Size)
-        std::unordered_map m(42); // expected-error-re{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
-    }
-    {
-        // cannot deduce Key from just (Size, Hash)
-        std::unordered_map m(42, std::hash<int>());
-            // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
-    }
-    {
-        // cannot deduce Key from just (Size, Hash, Pred)
-        std::unordered_map m(42, std::hash<int>(), std::equal_to<int>());
-            // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
-    }
-    {
-        // cannot deduce Key from just (Size, Hash, Pred, Allocator)
-        std::unordered_map m(42, std::hash<int>(), std::equal_to<int>(), std::allocator<P>());
-            // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
-    }
-    {
-        // cannot deduce Key from just (Allocator)
-        std::unordered_map m(std::allocator<P>{});
-            // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
-    }
-    {
-        // cannot deduce Key from just (Size, Allocator)
-        std::unordered_map m(42, std::allocator<P>());
-            // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
-    }
-    {
-        // cannot deduce Key from just (Size, Hash, Allocator)
-        std::unordered_map m(42, std::hash<int>(), std::allocator<P>());
-            // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
-    }
-
-  return 0;
+void test() {
+  using P = std::pair<const int, int>;
+  {
+    // cannot deduce Key from nothing
+    std::unordered_map m;
+      // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
+  }
+  {
+    // cannot deduce Key from just (Size)
+    std::unordered_map m(42);
+      // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Hash)
+    std::unordered_map m(42, std::hash<int>());
+      // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Hash, Pred)
+    std::unordered_map m(42, std::hash<int>(), std::equal_to<int>());
+      // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Hash, Pred, Allocator)
+    std::unordered_map m(42, std::hash<int>(), std::equal_to<int>(), std::allocator<P>());
+      // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
+  }
+  {
+    // cannot deduce Key from just (Allocator)
+    std::unordered_map m(std::allocator<P>{});
+      // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Allocator)
+    std::unordered_map m(42, std::allocator<P>());
+      // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
+  }
+  {
+    // cannot deduce Key from just (Size, Hash, Allocator)
+    std::unordered_map m(42, std::hash<int>(), std::allocator<P>());
+      // expected-error-re@-1{{no viable constructor or deduction guide for deduction of template arguments of '{{(std::)?}}unordered_map'}}
+  }
 }
