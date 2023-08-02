e4281af61e795cfbe3a3ba858be88f08b094d55d
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-08-02 01:31:05 -0400

    [libc++] [test] Reformat *.deduct.verify.cpp
    All tests have been reformatted to 2-space indents and commented
    in the modern style. Remove the repetition of Standardese deduction guides,
    since the important thing about these verify tests is that they reject
    the right things, not that they "exercise" specific signatures (which
    by definition they will not, because the point is that no specific signature
    matches, hence CTAD fails).

--- libcxx/test/std/containers/container.adaptors/queue/queue.cons/deduct.verify.cpp
+++ libcxx/test/std/containers/container.adaptors/queue/queue.cons/deduct.verify.cpp
@@ -6,41 +6,37 @@
 //
 //===----------------------------------------------------------------------===//
 
-// <queue>
 // UNSUPPORTED: c++03, c++11, c++14
+
+// <queue>
+
+// Test CTAD on cases where deduction should fail.
 
 #include <queue>
 #include <list>
-#include <iterator>
-#include <cassert>
-#include <cstddef>
+#include <memory>
 
-
-int main(int, char**)
+void test()
 {
-//  Test the explicit deduction guides
-    {
-//  queue(const Container&, const Alloc&);
-//  The '45' is not an allocator
-    std::queue que(std::list<int>{1,2,3}, 45);  // expected-error {{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
-    }
-
-    {
-//  queue(const queue&, const Alloc&);
-//  The '45' is not an allocator
+  {
+    // Cannot deduce T from nothing
+    std::queue q;
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
+  }
+  {
+    // Cannot deduce T from just (Alloc)
+    auto q = std::queue(std::allocator<int>());
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
+  }
+  {
+    // The '45' is not an allocator
+    auto q = std::queue(std::list<int>{1,2,3}, 45);
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
+  }
+  {
+    // The '45' is not an allocator
     std::queue<int> source;
-    std::queue que(source, 45);  // expected-error {{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
-    }
-
-//  Test the implicit deduction guides
-    {
-//  queue (allocator &)
-    std::queue que((std::allocator<int>()));  // expected-error {{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
-//  Note: The extra parens are necessary, since otherwise clang decides it is a function declaration.
-//  Also, we can't use {} instead of parens, because that constructs a
-//      stack<allocator<int>, allocator<allocator<int>>>
-    }
-
-
-  return 0;
+    auto q = std::queue(source, 45);
+        // expected-error@-1{{no viable constructor or deduction guide for deduction of template arguments of 'queue'}}
+  }
 }
