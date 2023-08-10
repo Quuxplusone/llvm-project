7b236884af32f29fcbe5b03575d08b256ef11664
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-08-10 11:05:32 -0400

    [clang] [C++] Implement "Deducing this" (P0847R7)
    Patch by Corentin Jabot!
    
    Differential Revision: https://reviews.llvm.org/D140828

--- clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
+++ clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
@@ -4451,7 +4451,7 @@ TemplateDeclInstantiator::SubstFunctionType(FunctionDecl *D,
   Qualifiers ThisTypeQuals;
   if (CXXMethodDecl *Method = dyn_cast<CXXMethodDecl>(D)) {
     ThisContext = cast<CXXRecordDecl>(Owner);
-    ThisTypeQuals = Method->getMethodQualifiers();
+    ThisTypeQuals = Method->getFunctionObjectParameterType().getQualifiers();
   }
 
   TypeSourceInfo *NewTInfo = SemaRef.SubstFunctionDeclType(
