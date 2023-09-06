1f44f26bbc69e7b922ca4559c41b33a7c091541d
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-09-06 17:49:47 -0400

    [libc++] Avoid using std::forward on things that aren't forwarding references
    See https://quuxplusone.github.io/blog/2023/05/27/dont-forward-non-forwarding-references/
    After this patch, it's possible to mechanically search-and-replace like this:
    `s/[_A-Za-z]*::forward<.*>[(](.*)[)]/_LIBCPP_FWD(\1)/g`
    and then `#define _LIBCPP_FWD(x) decltype(x)(x)`.
    We don't want to actually land that search-and-replace on trunk, but it's a nice
    property to have: to use `forward` only when we're actually forwarding, and not
    in any other situation.
    
    Differential Revision: https://reviews.llvm.org/D120998

--- libcxx/include/__memory/unique_ptr.h
+++ libcxx/include/__memory/unique_ptr.h
@@ -188,14 +188,14 @@ public:
   _LIBCPP_HIDE_FROM_ABI unique_ptr(pointer __p, _BadRValRefType<_Dummy> __d) = delete;
 
   _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX23 unique_ptr(unique_ptr&& __u) _NOEXCEPT
-      : __ptr_(__u.release(), std::forward<deleter_type>(__u.get_deleter())) {}
+      : __ptr_(__u.release(), static_cast<deleter_type&&>(__u.get_deleter())) {}
 
   template <class _Up,
             class _Ep,
             class = _EnableIfMoveConvertible<unique_ptr<_Up, _Ep>, _Up>,
             class = _EnableIfDeleterConvertible<_Ep> >
   _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX23 unique_ptr(unique_ptr<_Up, _Ep>&& __u) _NOEXCEPT
-      : __ptr_(__u.release(), std::forward<_Ep>(__u.get_deleter())) {}
+      : __ptr_(__u.release(), static_cast<_Ep&&>(__u.get_deleter())) {}
 
 #if _LIBCPP_STD_VER <= 14 || defined(_LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR)
   template <class _Up,
@@ -205,7 +205,7 @@ public:
 
   _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX23 unique_ptr& operator=(unique_ptr&& __u) _NOEXCEPT {
     reset(__u.release());
-    __ptr_.second() = std::forward<deleter_type>(__u.get_deleter());
+    __ptr_.second() = static_cast<deleter_type&&>(__u.get_deleter());
     return *this;
   }
 
@@ -215,7 +215,7 @@ public:
             class = _EnableIfDeleterAssignable<_Ep> >
   _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX23 unique_ptr& operator=(unique_ptr<_Up, _Ep>&& __u) _NOEXCEPT {
     reset(__u.release());
-    __ptr_.second() = std::forward<_Ep>(__u.get_deleter());
+    __ptr_.second() = static_cast<_Ep&&>(__u.get_deleter());
     return *this;
   }
 
@@ -372,11 +372,11 @@ public:
   _LIBCPP_HIDE_FROM_ABI unique_ptr(_Pp __p, _BadRValRefType<_Dummy> __d) = delete;
 
   _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX23 unique_ptr(unique_ptr&& __u) _NOEXCEPT
-      : __ptr_(__u.release(), std::forward<deleter_type>(__u.get_deleter())) {}
+      : __ptr_(__u.release(), static_cast<deleter_type&&>(__u.get_deleter())) {}
 
   _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX23 unique_ptr& operator=(unique_ptr&& __u) _NOEXCEPT {
     reset(__u.release());
-    __ptr_.second() = std::forward<deleter_type>(__u.get_deleter());
+    __ptr_.second() = static_cast<deleter_type&&>(__u.get_deleter());
     return *this;
   }
 
@@ -385,7 +385,7 @@ public:
             class = _EnableIfMoveConvertible<unique_ptr<_Up, _Ep>, _Up>,
             class = _EnableIfDeleterConvertible<_Ep> >
   _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX23 unique_ptr(unique_ptr<_Up, _Ep>&& __u) _NOEXCEPT
-      : __ptr_(__u.release(), std::forward<_Ep>(__u.get_deleter())) {}
+      : __ptr_(__u.release(), static_cast<_Ep&&>(__u.get_deleter())) {}
 
   template <class _Up,
             class _Ep,
@@ -393,7 +393,7 @@ public:
             class = _EnableIfDeleterAssignable<_Ep> >
   _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX23 unique_ptr& operator=(unique_ptr<_Up, _Ep>&& __u) _NOEXCEPT {
     reset(__u.release());
-    __ptr_.second() = std::forward<_Ep>(__u.get_deleter());
+    __ptr_.second() = static_cast<_Ep&&>(__u.get_deleter());
     return *this;
   }
 
