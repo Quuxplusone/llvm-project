779235ba70b35ec83b2729a628a558df67b081d2
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-09-07 12:14:57 -0400

    [clang] [P2953] Reject defaulted rvalue-ref-qualified operator=

--- clang/lib/Sema/SemaDeclCXX.cpp
+++ clang/lib/Sema/SemaDeclCXX.cpp
@@ -8090,7 +8090,7 @@ bool Sema::CheckExplicitlyDefaultedSpecialMember(CXXMethodDecl *MD,
       HadError = true;
     }
 
-    // A defaulted special member cannot have cv-qualifiers.
+    // A defaulted special member cannot have cv-qualifiers nor an rvalue-ref-qualifier.
     if (ThisType.isConstQualified() || ThisType.isVolatileQualified()) {
       if (DeleteOnTypeMismatch)
         ShouldDeleteForTypeMismatch = true;
@@ -8123,6 +8123,11 @@ bool Sema::CheckExplicitlyDefaultedSpecialMember(CXXMethodDecl *MD,
             << MD->getSourceRange();
         HadError = true;
       }
+    }
+    if (!HadError && MD->getFunctionObjectParameterReferenceType()->isRValueReferenceType()) {
+      Diag(MD->getLocation(), diag::err_defaulted_special_member_rref_qual)
+          << (CSM == CXXSpecialMemberKind::MoveAssignment);
+        HadError = true;
     }
   }
 
