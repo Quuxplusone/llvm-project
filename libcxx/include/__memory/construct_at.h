07cbd9e2726042ce29c0d930a2419518ec2a7879
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-09-27 10:53:13 -0400

    [libc++] Support std::__destroy_at(T[N]) in all language modes
    It's needed as a building block for std::relocate_at in all language modes,
    although it might not be useful for anything in trunk right now.

--- libcxx/include/__memory/construct_at.h
+++ libcxx/include/__memory/construct_at.h
@@ -54,8 +54,7 @@ _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 _Tp* __construct_at(_Tp* __l
 
 // destroy_at
 
-// The internal functions are available regardless of the language version (with the exception of the `__destroy_at`
-// taking an array).
+// The internal functions are available regardless of the language version.
 
 template <class _Tp, __enable_if_t<!is_array<_Tp>::value, int> = 0>
 _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 void __destroy_at(_Tp* __loc) {
@@ -63,7 +62,6 @@ _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 void __destroy_at(_Tp* __loc
   __loc->~_Tp();
 }
 
-#if _LIBCPP_STD_VER >= 20
 template <class _Tp, __enable_if_t<is_array<_Tp>::value, int> = 0>
 _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 void __destroy_at(_Tp* __loc) {
   _LIBCPP_ASSERT_NON_NULL(__loc != nullptr, "null pointer given to destroy_at");
@@ -71,7 +69,6 @@ _LIBCPP_HIDE_FROM_ABI _LIBCPP_CONSTEXPR_SINCE_CXX20 void __destroy_at(_Tp* __loc
   for (auto __it = std::begin(*__loc); __it != __end; ++__it)
     std::__destroy_at(__it);
 }
-#endif
 
 #if _LIBCPP_STD_VER >= 17
 
