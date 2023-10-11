c4f24ef6e8a7c9a6898f605beaa903cedf9cecf5
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-10-11 17:25:03 -0400

    [libc++] [P3016R1] Remove free begin/end/data/empty from <valarray> and <initializer_list>
    Put the change in only for C++26. It's actually tricky to put in globally,
    because in C++14 mode the old std::begin(initializer_list<E>) was constexpr-friendly
    but the generic std::begin(C&) wasn't constexpr-friendly; therefore the latter
    can't substitute for the former in C++14. We could probably do it in C++17
    and later; but as long as we're #ifdef'ing it at all, let's do it in the
    style that libc++ trunk would eventually end up doing it.
    
    I believe it would be difficult (i.e. require compiler magic that we don't
    currently have) to give a deprecation warning for the declarations in
    `<initializer_list>` but _not_ the declarations in `<iterator>`.

--- libcxx/include/__iterator/data.h
+++ libcxx/include/__iterator/data.h
@@ -12,7 +12,6 @@
 
 #include <__config>
 #include <cstddef>
-#include <initializer_list>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -35,11 +34,6 @@ constexpr _LIBCPP_HIDE_FROM_ABI auto data(const _Cont& __c) noexcept(noexcept(__
 template <class _Tp, size_t _Sz>
 _LIBCPP_HIDE_FROM_ABI constexpr _Tp* data(_Tp (&__array)[_Sz]) noexcept {
   return __array;
-}
-
-template <class _Ep>
-_LIBCPP_HIDE_FROM_ABI constexpr const _Ep* data(initializer_list<_Ep> __il) noexcept {
-  return __il.begin();
 }
 
 #endif
