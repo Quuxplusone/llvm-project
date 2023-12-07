4b8b88f3842fa292838fa5e5f8f3fb44a06e0ab5
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-12-07 18:13:54 -0500

    [libc++] [sort] Use branchless sort for all trivially copyable register-sized types
    For example, pointers and small POD structs.
    
    This changes the algorithm that `sort_stability.pass.cpp` uses for `EqualType`
    with a "simple comparator," but not if the comparator is an arbitrary lambda,
    so we adjust that test to use a simple comparator all the time.
    `nth_element_stability.pass.cpp` and `partial_sort_stability.pass.cpp` already
    use a simple comparator all the time, so they don't need to be adjusted.
    
    We considered forbidding the branchless version for unintentionally
    expensive comparators, e.g.
        double a[10]; std::sort(a, a+10, std::less<BigDouble>());
        Derived *a[10]; std::sort(a, a+10, std::less<VirtualBase*>());
    In both cases it is safe, but more expensive than the original PR
    intended (since the comparator will be invoked more often in the branchless
    version). However, it's invoked only 0.1% more often, and benchmarks show
    that the branchless version is still (much) faster despite the extra
    invocations. So we leave this alone.
    Sample benchmarks:
    - https://godbolt.org/z/x1Waz3vbY
    - https://godbolt.org/z/5Ph3hfqbc

--- libcxx/include/__algorithm/sort.h
+++ libcxx/include/__algorithm/sort.h
@@ -28,7 +28,6 @@
 #include <__iterator/iterator_traits.h>
 #include <__type_traits/conditional.h>
 #include <__type_traits/disjunction.h>
-#include <__type_traits/is_arithmetic.h>
 #include <__type_traits/is_constant_evaluated.h>
 #include <__type_traits/is_trivially_copyable.h>
 #include <__utility/move.h>
@@ -64,7 +63,7 @@ template <class _Compare, class _Iter, class _Tp = typename iterator_traits<_Ite
 using __use_branchless_sort =
     integral_constant<bool,
                       __libcpp_is_contiguous_iterator<_Iter>::value && sizeof(_Tp) <= sizeof(void*) &&
-                          is_arithmetic<_Tp>::value && __is_simple_comparator<_Compare>::value>;
+                          is_trivially_copyable<_Tp>::value && __is_simple_comparator<_Compare>::value>;
 
 namespace __detail {
 
