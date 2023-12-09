45c888f8bda41304bc2579ad29bc7c8df14553f0
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-12-09 12:53:49 -0500

    [clang] Remove isDependentType() from isTriviallyRelocatable(): does it ICE?
    I'd added isDependentType() a long time ago, but now I don't know why.
    Doesn't seem to ICE when I remove it, anyway.

--- clang/lib/AST/Type.cpp
+++ clang/lib/AST/Type.cpp
@@ -2662,7 +2662,7 @@ bool QualType::isTriviallyCopyConstructibleType(
 
 bool QualType::isTriviallyRelocatableType(const ASTContext &Context) const {
   QualType BaseElementType = Context.getBaseElementType(getCanonicalType());
-  if (BaseElementType->isIncompleteType() || BaseElementType->isDependentType()) {
+  if (BaseElementType->isIncompleteType()) {
     return false;
   } else if (!BaseElementType->isObjectType()) {
     return false;
