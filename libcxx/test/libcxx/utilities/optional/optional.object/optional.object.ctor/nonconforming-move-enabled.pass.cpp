f11aad93fe2030ecbc266c472016619938b15144
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2023-12-26 12:05:49 -0500

    [libc++] Implement _LIBCPP_NONCONFORMING_OPTIONAL_MOVE_CTOR
    The Standard requires that `optional`'s move constructor
    leave the right-hand side in a "moved-from, but still engaged" state,
    even when it would be cheaper to leave it in a "disengaged" state.
    We implement the latter behavior under a macro that can be turned
    on and off. It's off by default.
    
    This makes for inconsistent behavior, which is probably why C++17 chose
    to make the move-constructor never disengage the right-hand side. See the
    new test files for some examples.
    
    This was proposed in [P3055R1], and then removed again in [P3055R2]
    both to narrow the scope and because it's probably a bad idea to
    change it this long after the ship has sailed.

---
+++ libcxx/test/libcxx/utilities/optional/optional.object/optional.object.ctor/nonconforming-move-enabled.pass.cpp
@@ -0,0 +1,67 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+// ADDITIONAL_COMPILE_FLAGS: -D_LIBCPP_NONCONFORMING_OPTIONAL_MOVE_CTOR=1
+
+// <optional>
+
+// optional(optional<T>&& rhs);
+// Verify that the move constructor uses relocation for types that are trivially relocatable
+// but not trivially copyable, when _LIBCPP_NONCONFORMING_OPTIONAL_MOVE_CTOR is enabled.
+
+#include <cassert>
+#include <optional>
+#include <memory>
+#include <utility>
+
+#include "test_macros.h"
+
+int main(int, char**)
+{
+  {
+    std::optional<int> i = 42;
+    std::optional<int> j = std::move(i); // i remains engaged, because int is trivial
+    assert(i.has_value());
+    assert(i.value() == 42);
+    assert(j.has_value());
+    assert(j.value() == 42);
+  }
+  {
+    std::optional<std::shared_ptr<int>> i = std::make_shared<int>(42);
+    std::optional<std::shared_ptr<int>> j = std::move(i); // i becomes disengaged
+    assert(!i.has_value());
+    assert(j.has_value());
+    assert(j.value() != nullptr && *j.value() == 42);
+  }
+  {
+    std::optional<std::shared_ptr<int>> i = std::make_shared<int>(42);
+    std::optional<std::shared_ptr<void>> j = std::move(i); // i remains engaged, because T != U
+    assert(i.has_value());
+    assert(i.value() == nullptr);
+    assert(j.has_value());
+    assert(j.value() != nullptr && *(int*)j.value().get() == 42);
+  }
+  {
+    std::optional<std::unique_ptr<int>> i = std::make_unique<int>(42);
+    std::optional<std::shared_ptr<int>> j = std::move(i); // i remains engaged, because T != U
+    assert(i.has_value());
+    assert(i.value() == nullptr);
+    assert(j.has_value());
+    assert(j.value() != nullptr && *j.value() == 42);
+  }
+  {
+    std::optional<std::unique_ptr<int>> i = std::make_unique<int>(42);
+    std::optional<std::unique_ptr<int>> j = std::move(i); // i becomes disengaged
+    assert(!i.has_value());
+    assert(j.has_value());
+    assert(j.value() != nullptr && *j.value() == 42);
+  }
+
+  return 0;
+}
