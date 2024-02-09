19cbbf176f12d576fa670ec468b05546fbad9ac1
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2024-02-08 23:11:42 -0500

    [libc++] Trivially relocatable (3b/4): Optimize std::vector methods for trivially relocatable types.
    All of these operations are optimized now:
    
    - reallocation (e.g. `reserve`) (mainline already optimizes as of January 2024)
    - `insert`/`emplace` single
    - `insert` range
    - `erase` single
    - `erase` range

---
+++ libcxx/test/libcxx/containers/sequences/vector/emplace_trivially_relocatable.pass.cpp
@@ -0,0 +1,75 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03
+
+// <vector>
+
+// void emplace(const_iterator it, Args&&... args);
+
+#include <vector>
+#include <cassert>
+
+static int assignments = 0;
+
+template<bool TR>
+struct _LIBCPP_TRIVIALLY_RELOCATABLE_IF(TR) MyClass {
+    int value;
+    explicit MyClass(int i) : value(i) {}
+    MyClass(const MyClass& rhs) {
+        value = rhs.value;
+    }
+    MyClass(MyClass&& rhs) noexcept {
+        value = rhs.value;
+    }
+    MyClass& operator=(const MyClass& rhs) {
+        ++assignments;
+        value = rhs.value;
+        return *this;
+    }
+    MyClass& operator=(MyClass&& rhs) {
+        ++assignments;
+        value = rhs.value;
+        return *this;
+    }
+    ~MyClass() {}
+};
+
+template<bool TR>
+void test()
+{
+    using T = MyClass<TR>;
+    std::vector<T> vec;
+    vec.reserve(5);
+    vec.push_back(T(1));
+    vec.push_back(T(2));
+    vec.push_back(T(3));
+
+    assignments = 0;
+    vec.emplace(vec.begin() + 2, 4);
+    if (TR) {
+        assert(assignments == 0);  // relocate upward without assigning
+    } else {
+        assert(assignments >= 1);
+    }
+
+    assignments = 0;
+    vec.emplace(vec.begin() + 2, 5);
+    if (TR) {
+        assert(assignments == 0);  // relocate upward without assigning
+    } else {
+        assert(assignments >= 1);
+    }
+}
+
+int main(int, char**)
+{
+    test<true>();
+    test<false>();
+    return 0;
+}
