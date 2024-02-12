7449e6bb4dbed69afbdb538496401b045c590e16
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2024-02-12 00:53:29 -0500

    [clang] Implement the [[clang::trivially_relocatable]] attribute
    This works like [[clang::trivial_abi]], in that it marks the class as
    trivially relocatable regardless of its non-trivial special members.
    That is, the attribute warrants to the compiler that the programmer
    knows what they're doing (as in unique_ptr and vector).
    However, [[clang::trivially_relocatable]] doesn't affect ABI such as
    parameter-passing convention; it merely makes the builtin type trait
    `__is_trivially_relocatable(T)` return true.

--- clang/lib/Sema/SemaDeclCXX.cpp
+++ clang/lib/Sema/SemaDeclCXX.cpp
@@ -7324,6 +7324,23 @@ void Sema::CheckCompletedCXXClass(Scope *S, CXXRecordDecl *Record) {
       checkCUDADeviceBuiltinTextureClassTemplate(*this, Record);
   }
 
+  if (!Record->isDependentContext()) {
+    // Deal with [[trivially_relocatable(condition)]].
+    if (auto *TRA = Record->getAttr<TriviallyRelocatableAttr>()) {
+      if (Expr *Arg = TRA->getCond()) {
+        // Evaluate the condition. If it's ill-formed, ignore the attribute.
+        // If it's well-formed and true, render this type trivially relocatable.
+        SFINAETrap Trap(*this);
+        bool Result = false;
+        if (!Arg->EvaluateAsBooleanCondition(Result, Context) || !Result) {
+          // An ill-formed, non-constant, non-boolean, or false expression drops
+          // the attribute.
+          Record->dropAttr<TriviallyRelocatableAttr>();
+        }
+      }
+    }
+  }
+
   llvm::SmallDenseMap<OverloadedOperatorKind,
                       llvm::SmallVector<const FunctionDecl *, 2>, 4>
       TypeAwareDecls{{OO_New, {}},
