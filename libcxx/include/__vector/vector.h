3da36b0bf1c2853a89e410580ba494c2ca77fbb7
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2024-06-23 23:40:42 -0400

    [libc++] Optimize vector insert/erase to use trivial relocation for pmr::vector<pmr::string>
    But not insert/erase of vector<pmr::string>, because the allocators could differ.
    And not insert/erase of vector<AggContainingPmrString>, because all bets
    are off in that case.
    
    This changes the observable behavior of my existing test which explicitly
    tests the undefined-behavior cases (i.e. cases that break the PMR invariant
    of a `pmr::vector`). No defined-behavior case is affected.

--- libcxx/include/__vector/vector.h
+++ libcxx/include/__vector/vector.h
@@ -43,6 +43,7 @@
 #include <__memory/temp_value.h>
 #include <__memory/uninitialized_algorithms.h>
 #include <__memory/uninitialized_relocate.h>
+#include <__memory_resource/polymorphic_allocator.h>
 #include <__ranges/access.h>
 #include <__ranges/as_rvalue_view.h>
 #include <__ranges/concepts.h>
@@ -56,6 +57,7 @@
 #include <__type_traits/is_constructible.h>
 #include <__type_traits/is_nothrow_assignable.h>
 #include <__type_traits/is_nothrow_constructible.h>
+#include <__type_traits/is_pmr_relocatable_container.h>
 #include <__type_traits/is_pointer.h>
 #include <__type_traits/is_same.h>
 #include <__type_traits/is_swappable.h>
@@ -89,9 +91,8 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 template<class _Tp, class _Allocator>
 struct __vector_relocate_elements_via_memcpy : integral_constant<bool,
-  __libcpp_is_trivially_relocatable<_Tp>::value &&
-  __allocator_has_trivial_move_construct_v<_Allocator, _Tp> &&
-  __allocator_has_trivial_destroy_v<_Allocator, _Tp>
+  (__libcpp_is_trivially_relocatable<_Tp>::value && __allocator_has_trivial_move_construct_v<_Allocator, _Tp> && __allocator_has_trivial_destroy_v<_Allocator, _Tp>) ||
+  (__is_pmr_relocatable_container<_Tp>::value && __is_pmr_allocator<_Allocator>::value)
 > {};
 
 template <class _Allocator>
@@ -1164,7 +1165,7 @@ vector<_Tp, _Allocator>::erase(const_iterator __position) {
   pointer __p          = this->__layout_.__begin_ptr() + __ps;
   if (__vector_relocate_elements_via_memcpy<_Tp, _Allocator>::value && !__libcpp_is_constant_evaluated()) {
     __alloc_traits::destroy(this->__layout_.__alloc(), std::__to_address(__p));
-    std::uninitialized_relocate(__p + 1, this->__layout_.__end_ptr(), __p);
+    ::__builtin_memmove((void*)std::__to_address(__p), (void*)std::__to_address(__p + 1), (this->__layout_.__end_ptr() - (__p + 1)) * sizeof(_Tp));
     this->__destruct_at_end(this->__layout_.__end_ptr() - 1, true_type()); // destroy_via_noop
   } else {
     this->__destruct_at_end(std::move(__p + 1, this->__layout_.__end_ptr(), __p));
@@ -1183,7 +1184,7 @@ vector<_Tp, _Allocator>::erase(const_iterator __first, const_iterator __last) {
       for (pointer __q = __p; __q != __endp; ++__q) {
         __alloc_traits::destroy(this->__layout_.__alloc(), std::__to_address(__q));
       }
-      std::uninitialized_relocate(__endp, this->__layout_.__end_ptr(), __p);
+      ::__builtin_memmove((void*)std::__to_address(__p), (void*)std::__to_address(__endp), (this->__layout_.__end_ptr() - __endp) * sizeof(_Tp));
       this->__destruct_at_end(this->__layout_.__end_ptr() - (__last - __first), true_type()); // destroy_via_noop
     } else {
       this->__destruct_at_end(std::move(__p + (__last - __first), this->__layout_.__end_ptr(), __p));
