efd27a9be3bc791365b4c822b1e40dca2915cd9e
Mital Ashok <mital@mitalashok.co.uk>
2024-08-30 15:03:13 +0100

    [libc++] Implement `std::inplace_vector<T, N>`
    (cherry picked from commit 440c327975109e7fe0444467a45a93369b809c53)

--- libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
+++ libcxx/test/std/language.support/support.limits/support.limits.general/version.version.compile.pass.cpp
@@ -7376,17 +7376,11 @@
 #    error "__cpp_lib_initializer_list should have the value 202511L in c++26"
 #  endif
 
-#  if !defined(_LIBCPP_VERSION)
-#    ifndef __cpp_lib_inplace_vector
-#      error "__cpp_lib_inplace_vector should be defined in c++26"
-#    endif
-#    if __cpp_lib_inplace_vector != 202406L
-#      error "__cpp_lib_inplace_vector should have the value 202406L in c++26"
-#    endif
-#  else
-#    ifdef __cpp_lib_inplace_vector
-#      error "__cpp_lib_inplace_vector should not be defined because it is unimplemented in libc++!"
-#    endif
+#  ifndef __cpp_lib_inplace_vector
+#    error "__cpp_lib_inplace_vector should be defined in c++26"
+#  endif
+#  if __cpp_lib_inplace_vector != 202406L
+#    error "__cpp_lib_inplace_vector should have the value 202406L in c++26"
 #  endif
 
 #  ifndef __cpp_lib_int_pow2
